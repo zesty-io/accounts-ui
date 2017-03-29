e3b2bf26f42dec1bd6803abd5084c65c15a21cc6
stuart runyan <shrunyan@gmail.com>
2017-03-29 11:56:43 -0700

    WIP: react-router private routes

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
 import {Provider} from 'react-redux'
-import {Router, Route, browserHistory } from 'react-router'
+import {BrowserRouter, Redirect, Route} from 'react-router-dom'
 
 import {store} from './reducers'
 
@@ -10,10 +10,37 @@ import Dashboard from './views/dashboard'
 
 ReactDOM.render((
     <Provider store={store}>
-        <Router history={browserHistory}>
-            <Route component={App}>
-              {/* <Route path="/dashboard" component={Dashboard} /> */}
-            </Route>
-        </Router>
+        <BrowserRouter>
+            <PrivateRoute path="/z" component={App}>
+              <Route path="/dashboard" component={Dashboard} />
+            </PrivateRoute>
+        </BrowserRouter>
     </Provider>
 ), document.getElementById('app'))
+
+
+//
+const PrivateRoute = ({ component, ...rest }) => (
+  <Route {...rest} render={props => (
+    fakeAuth.isAuthenticated ? (
+      React.createElement(component, props)
+    ) : (
+      <Redirect to={{
+        pathname: '/login',
+        state: { from: props.location }
+      }}/>
+    )
+  )}/>
+)
+
+const fakeAuth = {
+  isAuthenticated: false,
+  authenticate(cb) {
+    this.isAuthenticated = true
+    setTimeout(cb, 100) // fake async
+  },
+  signout(cb) {
+    this.isAuthenticated = false
+    setTimeout(cb, 100)
+  }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -18,7 +18,7 @@ module.exports = {
               exclude: /(node_modules)/,
               loader: 'babel-loader',
               query: {
-                presets: ['es2015', 'react']
+                presets: ['react', 'es2015', 'stage-2']
               }
             }
         ]
