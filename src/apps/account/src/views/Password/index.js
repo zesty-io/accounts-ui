3d3797ec8d1d8d7cdd7da8528dd17f739f10c40f
Grant Glidewell <grant.glidewell@zesty.io>
2018-03-09 16:45:23 -0800

    password inputs type fo password

--- src/apps/account/src/views/Password/index.js
+++ src/apps/account/src/views/Password/index.js
@@ -6,9 +6,9 @@ class Password extends Component {
     return (
       <section id='settings'>
         <h2>Password</h2>
-        <div>Old Password </div><div><Input name='oldPassword' /></div>
-        <div>New Password </div><div><Input name='newPassword' /></div>
-        <div>Confirm New Password </div><div><Input name='confirmNewPassword' /></div>
+        <div>Old Password </div><div><Input name='oldPassword' type='password'/></div>
+        <div>New Password </div><div><Input name='newPassword' type='password'/></div>
+        <div>Confirm New Password </div><div><Input name='confirmNewPassword' type='password'/></div>
         <Button text="Submit" />
       </section>
     )
