84d40fa40a67dc94478825e119987c11abd19af9
stuart runyan <shrunyan@gmail.com>
2018-03-12 18:38:05 -0700

    fixed: make manager and preview urls

--- src/apps/properties/src/components/WebsiteCard/WebsiteCard.js
+++ src/apps/properties/src/components/WebsiteCard/WebsiteCard.js
@@ -1,61 +1,47 @@
 import React, { Component } from 'react'
-import {connect} from 'react-redux'
-import {Link} from 'react-router-dom'
+import { connect } from 'react-redux'
+import { Link } from 'react-router-dom'
 import cx from 'classnames'
 import styles from './WebsiteCard.less'
 
 class WebsiteCard extends Component {
-  render () {
+  render() {
     // console.log('site: ', this.props)
     return (
       <article className={styles.WebsiteCard}>
         <header>
-          <h1 className={styles.name}>
-            {/*<i className="fa fa-pencil" aria-hidden="true"></i>*/}
-            {this.props.site.name}
-          </h1>
-
-          {/*<h2 className={styles.domain}>
-            {this.props.site.domain
-              ? <a href={this.props.site.domain} target='_blank'>{this.props.site.domain}</a>
-              : <Button className={styles.setup}><i className={cx(styles.icon, 'fa fa-cog')} aria-hidden='true' />Setup Domain</Button>}
-            </h2>*/}
-
-          {/*
-          <ButtonGroup>
-            <Button>
-              Site Manager
-            </Button>
-            <Link>
-              <i className="fa fa-external-link" aria-hidden="true"></i>View Stage
-            </Link>
-            <Link>
-              <i className="fa fa-external-link" aria-hidden="true"></i>View Live
-            </Link>
-          </ButtonGroup>
-          */}
+          <h1 className={styles.name}>{this.props.site.name}</h1>
         </header>
         <main className={styles.WebsiteManage}>
-          {this.props.site.domain ?
-          <Url href="http://alphauniverse.com">
-            <i className="fa fa-globe" aria-hidden="true"></i>&nbsp;{this.props.site.domain}
-          </Url>
-          : null }
-          <p>
-            30 Day Requests: 1,300,000
-          </p>
-
+          {this.props.site.domain ? (
+            <Url href="http://alphauniverse.com">
+              <i className="fa fa-globe" aria-hidden="true" />&nbsp;{
+                this.props.site.domain
+              }
+            </Url>
+          ) : null}
+          <p>30 Day Requests: 1,300,000 TODO line graph</p>
         </main>
         <footer>
           <ButtonGroup className={styles.controls}>
-            <Button className={styles.manager}>
-              <i className="fa fa-external-link" aria-hidden="true"></i>Site Manager
-            </Button>
-            <Button>
-              <i className="fa fa-globe" aria-hidden="true"></i>Preview
-            </Button>
+            <Url
+              target="_blank"
+              href={`https://${this.props.site.zuid}.manage.zesty.io`}
+            >
+              <i className="fa fa-external-link" aria-hidden="true" />Site
+              Manager
+            </Url>
+            <Url
+              target="_blank"
+              href={`https://${this.props.site.zuid}.preview.zesty.io`}
+            >
+              <i className="fa fa-globe" aria-hidden="true" />Preview
+            </Url>
             <Link to={`/properties/${this.props.site.zuid}`}>
-              <i className={cx(styles.settings, "fa fa-cog")} aria-hidden="true"></i>
+              <i
+                className={cx(styles.settings, 'fa fa-cog')}
+                aria-hidden="true"
+              />
             </Link>
           </ButtonGroup>
         </footer>
