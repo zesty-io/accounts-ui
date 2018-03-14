53dea6f74922838db65c4626319f34ff9d3e2800
stuart runyan <shrunyan@gmail.com>
2018-03-13 20:51:28 -0700

    fixed: component naming

---
+++ src/shell/components/AppHeader/AppHeader.js
@@ -0,0 +1,32 @@
+import { PureComponent } from 'react'
+import { NavLink } from 'react-router-dom'
+import { MessageIcon } from '../Messages/Icon'
+import styles from './AppHeader.less'
+
+import { logout } from '../../store/auth'
+
+export default class AppHeader extends PureComponent {
+  render() {
+    return (
+      <header className={styles.AppHeader}>
+        <img className={styles.logo} src="/zesty-z-logo.png" />
+        <nav className={styles.GlobalNav}>
+          <NavLink to="/dashboard">Dashboard</NavLink>
+          <NavLink to="/properties">Web Properties</NavLink>
+          <NavLink to="/account/combined">My Account</NavLink>
+          <MessageIcon />
+        </nav>
+        <span className={styles.logout} title="Logout">
+          <span>
+            {this.props.user.firstname} {this.props.user.lastname}
+          </span>
+          <i
+            className="fa fa-sign-out"
+            aria-hidden="true"
+            onClick={() => this.props.dispatch(logout())}
+          />
+        </span>
+      </header>
+    )
+  }
+}
---
+++ src/shell/views/App/App.js
@@ -0,0 +1,57 @@
+import { Component } from 'react'
+import { connect } from 'react-redux'
+import { Redirect, Route, Switch } from 'react-router-dom'
+
+import Login from '../Login'
+import AppHeader from '../../components/AppHeader'
+import styles from './app.less'
+
+import { getUser } from '../../store/user'
+
+class PrivateRoute extends Component {
+  render() {
+    return this.props.loggedIn ? this.props.children : <Redirect to="/login" />
+  }
+}
+
+class Shell extends Component {
+  componentDidMount() {
+    // TODO how do I get the id?
+    this.props.dispatch(getUser(this.props.user.zuid))
+  }
+  render() {
+    return (
+      <section className={styles.shell}>
+        <AppHeader user={this.props.user} dispatch={this.props.dispatch} />
+        <Switch>
+          {/* <Route path='/dashboard' component={Dashboard} /> */}
+          <Route path="/properties" component={Properties} />
+          <Route path="/account" component={Account} />
+          <Route path="/dashboard" component={Dashboard} />
+          <Redirect from="/" to="/properties" />
+          {/* TODO: handle no match */}
+        </Switch>
+      </section>
+    )
+  }
+}
+let AppShell = connect(state => state)(Shell)
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <PrivateRoute loggedIn={this.props.auth.valid}>
+          <Route path="/" component={AppShell} />
+        </PrivateRoute>
+        <Switch>
+          <Route path="/login" component={Login} />
+          <Route path="/create-account" component={Login} />
+          <Route path="/reset-password" component={Login} />
+          <Route path="/verify-email" component={Login} />
+        </Switch>
+      </div>
+    )
+  }
+}
+export default connect(state => state)(App)
