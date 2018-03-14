b32029eb3824ec6dbfd62c1881964bd7f0511041
Grant Glidewell <grant.glidewell@zesty.io>
2018-03-13 20:36:11 -0700

    naming conventions, state passed down to subcomponents and rendering

--- src/apps/properties/src/components/website-view/Blueprint/index.js
+++ src/apps/properties/src/components/website-view/Blueprint/index.js
@@ -1,20 +1,2 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import { NavLink } from 'react-router-dom'
- 
-class Blueprint extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Blueprint</h3>        
-        <p>current blueprint: {this.props.site.blueprint}</p>
-        <img src={this.props.site.blueprintImgUrl} alt='blueprint Image' />
-        <p>{this.props.site.blueprintDetails}</p>
-        <a href='github.url'>View on Github</a>
-        <NavLink to='/account/blueprints'>Change Blueprint</NavLink>
-      </div>
-    )
-  }
-}
-
-export default connect(state => state)(Blueprint)
+import Blueprint from './Blueprint'
+export default Blueprint
--- src/apps/properties/src/components/website-view/Overview/index.js
+++ src/apps/properties/src/components/website-view/Overview/index.js
@@ -1,19 +1,2 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-
-class Overview extends Component {
-  render() {
-    return (
-      <div>
-        <h3>Overview</h3>
-        <p>Plan: {this.props.site.metadata.plan}</p>
-        <p>Contributors: {this.props.site.metadata.contributors}</p>
-        <p>Created On: {this.props.site.metadata.createdOn}</p>
-        <p>Created By: {this.props.site.metadata.createdBy}</p>
-        <p>Status: {this.props.site.metadata.status}</p>
-      </div>
-    )
-  }
-}
-
-export default connect(state => state)(Overview)
+import Overview from './Overview'
+export default Overview
