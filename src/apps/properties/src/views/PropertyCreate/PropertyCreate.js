2bfa20183362945a48a1a720f8d4b39d91aa9e71
stuart runyan <shrunyan@gmail.com>
2018-03-15 16:52:14 -0700

    fixed: prettier styling

--- src/apps/properties/src/views/PropertyCreate/PropertyCreate.js
+++ src/apps/properties/src/views/PropertyCreate/PropertyCreate.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
-import {bindActionCreators} from 'redux'
+import { bindActionCreators } from 'redux'
 import { Link } from 'react-router-dom'
 import styles from './PropertyCreate.less'
 
-import{ getBlueprints, addProperty } from '../../store'
+import { getBlueprints, addProperty } from '../../store'
 
 class PropertyCreate extends Component {
   constructor(props) {
@@ -22,42 +22,57 @@ class PropertyCreate extends Component {
     evt.preventDefault()
     console.log(this.state)
   }
-  handleChange = (evt) => {
+  handleChange = evt => {
     this.setState({ name: evt.target.value })
   }
   render() {
     return (
       <section className={styles.PropertyCreate}>
-        {!this.state.bpStep
-          ? <div className={styles.nameNew}>
-              <h3>Name your new web property</h3>
-              <Input type='text' name='newPropertName' value={this.state.name} onChange={this.handleChange}/>
-              <Button name='submitNewPropertyName' text='Submit' onClick={() => this.setState({ bpStep: true })} />
-              <Link to='/properties'> <Button name='cancel' text='cancel' /> </Link>
-            </div>
-          : <div>
-              {
-                this.props.sites.blueprints.map((blueprint, i) => {
-                  return (<div key={i}>
-                    <h2>{blueprint.name}</h2>
-                    <p>{blueprint.description}</p>
-                    <img src={blueprint.url} alt='bp img' />
-                    <Button name={`selectBlueprint${i}`} text='select this blueprint' /></div>
-                  )
-                })
-              }
-            </div>
-        }
-
+        {!this.state.bpStep ? (
+          <div className={styles.nameNew}>
+            <h3>Name your new web property</h3>
+            <Input
+              type="text"
+              name="newPropertName"
+              value={this.state.name}
+              onChange={this.handleChange}
+            />
+            <Button
+              name="submitNewPropertyName"
+              text="Submit"
+              onClick={() => this.setState({ bpStep: true })}
+            />
+            <Link to="/properties">
+              {' '}
+              <Button name="cancel" text="cancel" />{' '}
+            </Link>
+          </div>
+        ) : (
+          <div>
+            {this.props.sites.blueprints.map((blueprint, i) => {
+              return (
+                <div key={i}>
+                  <h2>{blueprint.name}</h2>
+                  <p>{blueprint.description}</p>
+                  <img src={blueprint.url} alt="bp img" />
+                  <Button
+                    name={`selectBlueprint${i}`}
+                    text="select this blueprint"
+                  />
+                </div>
+              )
+            })}
+          </div>
+        )}
       </section>
     )
   }
 }
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return state
 }
 const mapDispatchToProps = state => {
-  return bindActionCreators({getBlueprints, addProperty}, dispatch)
+  return bindActionCreators({ getBlueprints, addProperty }, dispatch)
 }
 
 export default connect(mapStateToProps)(PropertyCreate)
