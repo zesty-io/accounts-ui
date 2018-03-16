1c7d66106bc5c532ed7962e94372da6d520290d4
stuart runyan <shrunyan@gmail.com>
2018-03-16 10:46:55 -0700

    do not require name on input component

--- src/core/input/Input.js
+++ src/core/input/Input.js
@@ -3,20 +3,8 @@ import styles from './Input.less'
 import cx from 'classnames'
 
 export default function Input(props) {
-  // let opts = {
-  //   'className': cx(styles.button, props.className),
-  //   'data-data': props.data,
-  //   'onClick': props.onClick
-  // }
-  // if (props.disabled) {
-  //   opts['disabled'] = 'disabled'
-  // }
-  if(!props.name){
-    console.error("Input component requires a 'name' attribute")
-    return <b>**INPUT COMPONENT REQUIRES A NAME ATTRIBUTE**</b>
-  }else{
-    return (
-      <input {...props} className={cx(styles.Input, props.className)} />
-    )
+  if (props.disabled) {
+    props['disabled'] = 'disabled'
   }
+  return <input {...props} className={cx(styles.Input, props.className)} />
 }
