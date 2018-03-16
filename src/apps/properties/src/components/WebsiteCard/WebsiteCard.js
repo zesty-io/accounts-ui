f424af2df71b26d93ec7ef6fa18a65416cedaf39
stuart runyan <shrunyan@gmail.com>
2018-03-16 11:25:05 -0700

    WIP: web property cards

--- src/apps/properties/src/components/WebsiteCard/WebsiteCard.js
+++ src/apps/properties/src/components/WebsiteCard/WebsiteCard.js
@@ -13,11 +13,11 @@ class WebsiteCard extends Component {
       datasets: [
         {
           label: 'requests',
-          data: Array.from({length:7}, () => Math.floor(Math.random() * 7))
+          data: Array.from({ length: 7 }, () => Math.floor(Math.random() * 7))
         },
         {
           label: 'media',
-          data: Array.from({length:7}, () => Math.floor(Math.random() * 7))
+          data: Array.from({ length: 7 }, () => Math.floor(Math.random() * 7))
         }
       ]
     }
@@ -33,19 +33,43 @@ class WebsiteCard extends Component {
     return (
       <article className={styles.WebsiteCard}>
         <header>
-          <h1 className={styles.name}>{this.props.site.AccountName}</h1>
+          <div className={styles.name}>
+            <h1>{this.props.site.AccountName}</h1>
+            {this.props.site.Domain ? (
+              <Url target="_blank" href={`http://${this.props.site.Domain}`}>
+                <i className="fa fa-globe" aria-hidden="true" />&nbsp;{
+                  this.props.site.Domain
+                }
+              </Url>
+            ) : null}
+          </div>
+
+          <Url
+            target="_blank"
+            href={`https://${this.props.site.RandomHashID}.preview.zesty.io`}
+          >
+            <i className="fa fa-globe" aria-hidden="true" /> Preview
+          </Url>
         </header>
         <main className={styles.WebsiteManage}>
           <Line data={data} options={options} />
         </main>
         <footer>
-          {this.props.site.Domain ? (
-            <Url target="_blank" href={`http://${this.props.site.Domain}`}>
-              <i className="fa fa-globe" aria-hidden="true" />&nbsp;{
-                this.props.site.Domain
-              }
+          {/* <ButtonGroup className={styles.controls}>
+            <Url
+              target="_blank"
+              href={`https://${this.props.site.RandomHashID}.preview.zesty.io`}
+            >
+              <i className="fa fa-globe" aria-hidden="true" /> Preview
             </Url>
-          ) : null}
+            {this.props.site.Domain ? (
+              <Url target="_blank" href={`http://${this.props.site.Domain}`}>
+                <i className="fa fa-globe" aria-hidden="true" />&nbsp;{
+                  this.props.site.Domain
+                }
+              </Url>
+            ) : null}
+          </ButtonGroup> */}
           <ButtonGroup className={styles.controls}>
             <Url
               target="_blank"
@@ -54,12 +78,12 @@ class WebsiteCard extends Component {
               <i className="fa fa-external-link" aria-hidden="true" /> Site
               Manager
             </Url>
-            <Url
+            {/* <Url
               target="_blank"
               href={`https://${this.props.site.RandomHashID}.preview.zesty.io`}
             >
               <i className="fa fa-globe" aria-hidden="true" /> Preview
-            </Url>
+            </Url> */}
             <Link to={`/properties/${this.props.site.ZUID}`}>
               <i
                 className={cx(styles.settings, 'fa fa-cog')}
