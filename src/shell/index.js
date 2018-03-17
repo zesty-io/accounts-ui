610fe810f75bd971a835865bfb18ab4b415465c0
stuart runyan <shrunyan@gmail.com>
2018-03-17 10:03:44 -0700

    fixed: changed root background color to the lighter app background to better match the feeling of light and zesty

--- src/shell/index.js
+++ src/shell/index.js
@@ -1,22 +1,23 @@
 import ReactDOM from 'react-dom'
-import {Provider} from 'react-redux'
-import {BrowserRouter, Route} from 'react-router-dom'
+import { Provider } from 'react-redux'
+import { BrowserRouter, Route } from 'react-router-dom'
 
-import {store} from './store'
-import {verifyAuth} from './store/auth'
+import { store } from './store'
+import { verifyAuth } from './store/auth'
 
 import App from './views/App'
 
 let unsubscribe = store.subscribe(() => {
   let state = store.getState()
   if (!state.auth.checking) {
-    ReactDOM.render((
+    ReactDOM.render(
       <Provider store={store}>
         <BrowserRouter>
           <Route component={App} />
         </BrowserRouter>
-      </Provider>
-    ), document.getElementById('root'))
+      </Provider>,
+      document.getElementById('root')
+    )
   }
 })
 
