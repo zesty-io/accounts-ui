739978d48cb4949aaad2b832f952894d48ad1eb5
Grant Glidewell <grant.glidewell@zesty.io>
2018-03-16 17:26:19 -0700

    formatting blueprint

--- src/apps/properties/src/components/WebsiteOverview/Blueprint/Blueprint.js
+++ src/apps/properties/src/components/WebsiteOverview/Blueprint/Blueprint.js
@@ -1,21 +1,29 @@
-import React, {Component} from 'react'
-import {connect} from 'react-redux'
-import { NavLink } from 'react-router-dom'
- 
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { NavLink } from "react-router-dom";
+
 class Blueprint extends Component {
   render() {
-    return this.props.blueprints[this.props.site.BlueprintID] instanceof Object 
-      ? <React.Fragment>
-        <img src={this.props.blueprints[this.props.site.BlueprintID].MainImage}
-          style={{maxHeight:'64px', maxWidth:'64px'}}
-          alt='blueprint Image' />
-        current blueprint: {this.props.blueprints[this.props.site.BlueprintID].Name}
+    return this.props.blueprints[this.props.site.BlueprintID] instanceof
+      Object ? (
+      <React.Fragment>
+        <img
+          src={this.props.blueprints[this.props.site.BlueprintID].MainImage}
+          style={{ maxHeight: "64px", maxWidth: "64px" }}
+          alt="blueprint Image"
+        />
+        current blueprint:{" "}
+        {this.props.blueprints[this.props.site.BlueprintID].Name}
         {this.props.blueprints[this.props.site.BlueprintID].Description}
-        <a href={this.props.blueprints[this.props.site.BlueprintID].PreviewURL}>View Preview</a>
-        <NavLink to='/account/blueprints'>Change Blueprint</NavLink>
+        <a href={this.props.blueprints[this.props.site.BlueprintID].PreviewURL}>
+          View Preview
+        </a>
+        <NavLink to="/account/blueprints">Change Blueprint</NavLink>
       </React.Fragment>
-        : <p>loading</p>
+    ) : (
+      <p>loading</p>
+    );
   }
 }
 
-export default connect(state => state)(Blueprint)
+export default connect(state => state)(Blueprint);
--- src/apps/properties/src/components/WebsiteOverview/UserAccess/UserAccess.js
+++ src/apps/properties/src/components/WebsiteOverview/UserAccess/UserAccess.js
@@ -1,39 +1,40 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import styles from './style.less'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import styles from "./style.less";
 class UserAccess extends Component {
-  componentWillUnmount(){
-    this.props.dispatch({ type: 'CLEAR_USERS' })
+  componentWillUnmount() {
+    this.props.dispatch({ type: "CLEAR_USERS" });
   }
   render() {
     return (
       <div className={styles.userAccess}>
-        <Input name='newUser' type='text' />
-        <Button name='newUserSubmit'>Send Invite</Button>
-        <div className ={styles.userTable}>
-        <header>
-                <h3>First Name</h3>
-                <h3>Last Name</h3>
-                <h3>Email</h3>
-                <h3>Staff</h3>
-              </header>
-              <main>
-              {Array.isArray(this.props.sitesUsers)
+        <Input name="newUser" type="text" />
+        <Button name="newUserSubmit">Send Invite</Button>
+        <div className={styles.userTable}>
+          <header>
+            <h3>First Name</h3>
+            <h3>Last Name</h3>
+            <h3>Email</h3>
+            <h3>Staff</h3>
+          </header>
+          <main>
+            {Array.isArray(this.props.sitesUsers)
               ? this.props.sitesUsers.map((user, i) => {
-                  return (<article key={i}>
-                    <span>{user.firstName} </span>
-                    <span>{user.lastName} </span>
-                    <span>{user.email} </span>
-                    <span>{user.staff}</span>
-                  </article>)
-              })
-              : null
-            }
-              </main>
-              </div>
+                  return (
+                    <article key={i}>
+                      <span>{user.firstName} </span>
+                      <span>{user.lastName} </span>
+                      <span>{user.email} </span>
+                      <span>{user.staff}</span>
+                    </article>
+                  );
+                })
+              : null}
+          </main>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default connect(state => state)(UserAccess)
+export default connect(state => state)(UserAccess);
