67812dfcb2ff393901868c356b2ada676877d813
Grant Glidewell <grant.glidewell@zesty.io>
2018-03-23 15:27:03 -0700

    added: styles for edit/create blueprint in accounts started, still ugly but cleaned up quite a bit

--- src/apps/account/src/views/EditBlueprint/EditBlueprint.js
+++ src/apps/account/src/views/EditBlueprint/EditBlueprint.js
@@ -5,171 +5,162 @@ import { connect } from "react-redux";
 import styles from "./EditBlueprint.less";
 
 class EditBlueprint extends Component {
-	handleSubmit = (evt) => {
-		evt.preventDefault()
-		console.log('WEEEEEEEEE!') // disabled for now
-	}
+  handleSubmit = evt => {
+    evt.preventDefault();
+    console.log("WEEEEEEEEE!"); // disabled for now
+  };
   render() {
-		let { blueprint } = this.props
+    let { blueprint } = this.props;
     return this.props && this.props.blueprint ? (
-      <div className={styles.blueprints}>
-        <form id="edit-form" onSubmit={this.handleSubmit}>
-          <div>
-            <div>
-              <img
-                src={`https://raw.githubusercontent.com/${
-                  blueprint.Name
-                }/master/shield.png`}
-                alt={blueprint.Name}
-              />
-            </div>
-
-            <div>
-              <fieldset>
-                <label>Blueprint Name</label>
-                <Input
-                  type="text"
-                  placeholder={blueprint.Name}
-                  name="edit[name]"
-                />
-                <label>Github Repo URL</label>
-                <Input
-                  type="text"
-                  placeholder={blueprint.GithubURL}
-                  name="edit[github_url]"
-                />
-                <label>Blueprint Example Preview URL</label>
-                <Input
-                  type="text"
-                  placeholder={blueprint.PreviewURL}
-                  name="edit[preview_url]"
-                />
-                <br />
-                <br />
-                <a href={`/some way to the creator/${blueprint.ID}/`}>View Plate in Creator</a>
-              </fieldset>
-            </div>
-            <div>
-              <label>
-                Shield Image URL (Optional. This will override shield.png in
-                your repo.)
-              </label>
-              <Input type="text" value={blueprint.MainImage} name="edit[main_image]" />
-
-              <label>
-                Background Cover Image URL (Optional. This will override
-                shield.png in your repo.)
-              </label>
-              <Input type="text" value={blueprint.CoverImage} name="edit[cover_image]" />
-            </div>
+      <form id="edit-form" onSubmit={this.handleSubmit}>
+        <div className={styles.blueprints}>
+          <div className={styles.rowOne}>
+            <img
+              src={`https://raw.githubusercontent.com/${
+                blueprint.Name
+              }/master/shield.png`}
+              alt={blueprint.Name}
+            />
+            <label>Blueprint Name</label>
+            <Input type="text" placeholder={blueprint.Name} name="edit[name]" />
+            <label>Github Repo URL</label>
+            <Input
+              type="text"
+              placeholder={blueprint.GithubURL}
+              name="edit[github_url]"
+            />
+            <label>Blueprint Example Preview URL</label>
+            <Input
+              type="text"
+              placeholder={blueprint.PreviewURL}
+              name="edit[preview_url]"
+            />
+            <a href={`/some way to the creator/${blueprint.ID}/`}>
+              View Plate in Creator
+            </a>
+            <label>
+              Shield Image URL (Optional. This will override shield.png in your
+              repo.)
+            </label>
+            <Input
+              type="text"
+              placeholder={blueprint.MainImage}
+              name="edit[main_image]"
+            />
+            <label>
+              Background Cover Image URL (Optional. This will override
+              shield.png in your repo.)
+            </label>
+            <Input
+              type="text"
+              placeholder={blueprint.CoverImage}
+              name="edit[cover_image]"
+            />
           </div>
-          <hr />
-          <div>
-            <div>
-              <label>Short Description</label>
-              <textarea name="edit[short_description]" value={blueprint.ShortDescription} />
-            </div>
-            <div>
-              <label>Description</label>
-              <textarea name="edit[description]" value={blueprint.Description} />
-            </div>
+          <div className={styles.rowTwo}>
+            <label>Short Description</label>
+            <textarea
+              rows={10}
+              cols={25}
+              wrap="soft"
+              name="edit[short_description]"
+              placeholder={blueprint.ShortDescription}
+            />
           </div>
-          <hr />
-
-          <label>Tags</label>
-          <div>
+          <div className={styles.rowTwo1}>
+            <label>Description</label>
+            <textarea
+              rows={20}
+              cols={35}
+              wrap="soft"
+              name="edit[description]"
+              placeholder={blueprint.Description}
+            />
+          </div>
+          <div className={styles.rowThree}>
+            <label>Tags</label>
             <Input type="hidden" name="edit[tags_csv][]" value="" />
-            <div>
-              <h4>Categories</h4>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="18" />Frameworks
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="3" />Bootstrap
-                3.5
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="1" />Zesty
-                Classic Framework
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="2" />Landing
-                Pages
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="4" />Developer
-                Starts
-              </label>
-            </div>
-            <div>
-              <h4>Descriptive</h4>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="19" />Colorful
-              </label>
-            </div>
-            <div>
-              <h4>Trends</h4>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="20" />Flat
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="21" />Bootstrap
-              </label>
-            </div>
-            <div>
-              <h4>Property Traits</h4>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="6" />Mobile
-                Responsive
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="7" />Social
-                Share Buttons
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="8" />Facebook
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="9" />Instagram
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="10" />Twitter
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="11" />Google+
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="12" />Authorship
-              </label>
-            </div>
-            <div>
-              <h4>Content Traits</h4>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="24" />Single
-                Page
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="25" />Multi
-                Page
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="26" />Blogs
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="27" />Galleries
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="28" />Menus
-              </label>
-              <label>
-                <Input type="checkbox" name="edit[tags_csv][]" value="29" />Business
-              </label>
-            </div>
+            <h4>Categories</h4>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="18" />Frameworks
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="3" />Bootstrap
+              3.5
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="1" />Zesty
+              Classic Framework
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="2" />Landing
+              Pages
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="4" />Developer
+              Starts
+            </label>
+            <h4>Descriptive</h4>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="19" />Colorful
+            </label>
+            <h4>Trends</h4>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="20" />Flat
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="21" />Bootstrap
+            </label>
+            <h4>Property Traits</h4>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="6" />Mobile
+              Responsive
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="7" />Social
+              Share Buttons
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="8" />Facebook
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="9" />Instagram
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="10" />Twitter
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="11" />Google+
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="12" />Authorship
+            </label>
+            <h4>Content Traits</h4>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="24" />Single
+              Page
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="25" />Multi
+              Page
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="26" />Blogs
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="27" />Galleries
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="28" />Menus
+            </label>
+            <label>
+              <Input type="checkbox" name="edit[tags_csv][]" value="29" />Business
+            </label>
+
+            <Button type="submit" text="Save" />
           </div>
-          <hr />
-          <Button type="submit" text="Save" />
-        </form>
-      </div>
+        </div>
+      </form>
     ) : (
       <p>No Blueprint Found</p>
     );
