7a7709132e576664d261b62a818d12a772efb77c
Grant Glidewell <grant.glidewell@zesty.io>
2018-04-16 16:09:30 -0700

    WIP: invite cards, fixed: verify overwriting user ZUID with null

--- src/apps/properties/src/components/WebsiteInvite/WebsiteInvite.js
+++ src/apps/properties/src/components/WebsiteInvite/WebsiteInvite.js
@@ -1,39 +1,23 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { Link, withRouter } from 'react-router-dom'
+import React, { Component } from "react";
+import { connect } from "react-redux";
+import { Link, withRouter } from "react-router-dom";
 
-import cx from 'classnames'
-import styles from './WebsiteInvite.less'
+import cx from "classnames";
+import styles from "./WebsiteInvite.less";
 
-import { Line } from 'react-chartjs-2'
+import { Line } from "react-chartjs-2";
 
 class WebsiteInvite extends Component {
-  handleClick = evt => {
+  handleAccept = evt => {
     // post accepted invite data THEN route to the overview when the user has permissions
-    this.props.history.push(`/properties/${this.props.site.ZUID}`)
-  }
+    // this.props.history.push(`/properties/${this.props.site.ZUID}`)
+    console.log(this.props);
+  };
+  handleDecline = evt => {
+    // post decline and re-fetch sites
+    console.log(this.props);
+  };
   render() {
-    // let data = {
-    //   labels: ['sun', 'mon', 'tues', 'wed', 'thur', 'fri', 'sat'],
-    //   datasets: [
-    //     {
-    //       label: 'requests',
-    //       data: [2, 4, 0, 6, 2, 4, 9]
-    //     },
-    //     {
-    //       label: 'media',
-    //       data: [3, 6, 11, 7, 3, 6, 11]
-    //     }
-    //   ]
-    // }
-
-    // let options = {
-    //   elements: {
-    //     line: {
-    //       tension: 0 // disables bezier curves
-    //     }
-    //   }
-    // }
     return (
       <article className={styles.WebsiteInvite}>
         <header>
@@ -47,32 +31,28 @@ class WebsiteInvite extends Component {
           ) : null}
         </header>
         <main className={styles.WebsiteManage}>
-          {/*<Line data={data} options={options} />*/}
           <Url
             className={styles.preview}
             target="_blank"
             title={`Preview  ${this.props.site.name}`}
             href={`https://${this.props.site.RandomHashID}.preview.zesty.io`}
           >
-            <i className={cx(styles.icon, 'fa fa-globe')} aria-hidden="true" />
+            <i className={cx(styles.icon, "fa fa-globe")} aria-hidden="true" />
           </Url>
         </main>
         <footer>
-          {/* {this.props.site.Domain ? (
-            <Url target="_blank" href={`http://${this.props.site.Domain}`}>
-              <i className="fa fa-globe" aria-hidden="true" />&nbsp;{
-                this.props.site.Domain
-              }
-            </Url>
-          ) : null} */}
-          <Button className={styles.invite} onClick={this.handleClick}>
+          <Button className={styles.invite} onClick={this.handleAccept}>
             <i className="fa fa-check-circle-o" aria-hidden="true" />
             Accept Invite
           </Button>
+          <Button className={styles.invite} onClick={this.handleDecline}>
+            <i className="fa fa-times-circle" aria-hidden="true" />
+            Decline Invite
+          </Button>
         </footer>
       </article>
-    )
+    );
   }
 }
 
-export default withRouter(connect(state => state)(WebsiteInvite))
+export default withRouter(connect(state => state)(WebsiteInvite));
--- src/apps/properties/src/store/sites.js
+++ src/apps/properties/src/store/sites.js
@@ -52,6 +52,44 @@ export function sites(state = {}, action) {
         updatedAt: "2015-05-01T16:31:41Z",
         deletedAt: null
       };
+      sites["0-test-invite2"] = {
+        invite: true,
+        ID: 7353800,
+        ZUID: "8-45a194a-1zg0cg",
+        EcoID: 144,
+        PropertyTypeID: 1,
+        RandomHashID: "385b14d47412fe6c30292f53473d62a5",
+        Domain: "stuartrunyan.com",
+        DomainVerified: true,
+        DevelopmentWebsite: false,
+        Name: "NewSite.com",
+        LegacyDiscountReason: null,
+        PlanID: 14,
+        Addons: null,
+        UpdatedDate: "2017-10-26T20:16:24Z",
+        PaymentMethod: null,
+        Internal: false,
+        Cancelled: false,
+        CancelledReason: null,
+        CancelledCustomerReason: null,
+        CancelledDate: null,
+        Zesty2Account: false,
+        ContainerNameOverride: null,
+        CDNURL: "https://58h1pb.media.zestyio.com",
+        Plugins: null,
+        EarlyAccessTier: 0,
+        DateActive: null,
+        CreatedDatetime: "2015-04-29T13:08:46Z",
+        CreatedByUserID: 20472736,
+        ThirdPartyOAuthTokens: '{"google":"421"}',
+        Favicon: null,
+        Referrer: "https://zesty.io/",
+        BlueprintID: 15,
+        RequiresTwoFactor: 0,
+        createdAt: null,
+        updatedAt: "2015-05-01T16:31:41Z",
+        deletedAt: null
+      };
 
       return sites;
     // return {}
