b0ed59fca27cbbbb879766295c9af84de467b3e8
stuart runyan <shrunyan@gmail.com>
2018-04-17 14:54:09 -0700

    styled global nav to have user section

--- src/shell/components/AppHeader/AppHeader.js
+++ src/shell/components/AppHeader/AppHeader.js
@@ -1,6 +1,5 @@
 import { PureComponent } from 'react'
 import { NavLink } from 'react-router-dom'
-import { MessageIcon } from '../Messages/Icon'
 import styles from './AppHeader.less'
 
 import { logout } from '../../store/auth'
@@ -11,22 +10,35 @@ export default class AppHeader extends PureComponent {
       <header className={styles.AppHeader}>
         <img className={styles.logo} src="/zesty-z-logo.svg" />
         <nav className={styles.GlobalNav}>
-          {/* <NavLink to="/dashboard">Dashboard</NavLink> */}
           <NavLink to="/properties">Web Properties</NavLink>
-          <NavLink to="/settings/account">My Account</NavLink>
         </nav>
-        <span
-          className={styles.logout}
-          title="Logout"
-          onClick={() => this.props.dispatch(logout())}
-        >
+        <nav className={styles.UserNav}>
+          <img
+            className={styles.avatar}
+            src={`https://www.gravatar.com/avatar/${
+              this.props.user.email
+            }?d=mm&s=30`}
+          />
           <span className={styles.name}>
             {this.props.user.firstName} {this.props.user.lastName}
           </span>
-          <span>
-            <i className="fa fa-sign-out" aria-hidden="true" /> Logout
-          </span>
-        </span>
+          <ul className={styles.UserMenu}>
+            <li>
+              <NavLink to="/settings/account">
+                <i className="fa fa-cog" aria-hidden="true" />
+                &nbsp;My Account
+              </NavLink>
+            </li>
+            <li
+              className={styles.logout}
+              title="Logout"
+              onClick={() => this.props.dispatch(logout())}
+            >
+              <i className="fa fa-sign-out" aria-hidden="true" />
+              &nbsp;Logout
+            </li>
+          </ul>
+        </nav>
       </header>
     )
   }
