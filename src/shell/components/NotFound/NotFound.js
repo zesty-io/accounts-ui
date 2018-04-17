e03e2c8b5f0c01a8c9cea18f70e5df9c2aebef17
Grant Glidewell <grant.glidewell@zesty.io>
2018-04-17 13:37:18 -0700

    added: basic redirect in notFound component and styles

--- src/shell/components/NotFound/NotFound.js
+++ src/shell/components/NotFound/NotFound.js
@@ -1,7 +1,14 @@
 import React from "react";
+import styles from "./NotFound.less";
 
 const NotFound = () => {
-  return <div>Page Not Found</div>;
+  window.setTimeout(() => (window.location = "/login"), 3000);
+  return (
+    <div className={styles.notFound}>
+      <h1>This is not the page you are looking for</h1>
+      <h2>taking you to a safe place</h2>
+    </div>
+  );
 };
 
 export default NotFound;
