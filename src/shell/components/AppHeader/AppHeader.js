526a340abab5b6c257b413c3ab7376f4935870a6
stuart runyan <shrunyan@gmail.com>
2018-04-18 13:59:19 -0700

    more work on user global nav

--- src/shell/components/AppHeader/AppHeader.js
+++ src/shell/components/AppHeader/AppHeader.js
@@ -1,5 +1,6 @@
 import { PureComponent } from 'react'
 import { NavLink } from 'react-router-dom'
+import cx from 'classnames'
 import styles from './AppHeader.less'
 
 import { logout } from '../../store/auth'
@@ -12,17 +13,24 @@ export default class AppHeader extends PureComponent {
         <nav className={styles.GlobalNav}>
           <NavLink to="/properties">Web Properties</NavLink>
         </nav>
+        <nav className={styles.HelpNav}>
+          <a href="https://developer.zesty.io" target="_blank">
+            <i className={cx(styles.icon, "fa fa-question-circle")} aria-hidden="true"></i>
+          </a>
+        </nav>
         <nav className={styles.UserNav}>
           <img
             className={styles.avatar}
             src={`https://www.gravatar.com/avatar/${
-              this.props.user.email
+              this.props.user.emailHash
             }?d=mm&s=30`}
           />
-          <span className={styles.name}>
-            {this.props.user.firstName} {this.props.user.lastName}
-          </span>
+
           <ul className={styles.UserMenu}>
+            <li className={styles.user}>
+              <span className={styles.name}>{this.props.user.firstName} {this.props.user.lastName}</span>
+              <span className={styles.email}>{this.props.user.email}</span>
+            </li>
             <li>
               <NavLink to="/settings/account">
                 <i className="fa fa-cog" aria-hidden="true" />
