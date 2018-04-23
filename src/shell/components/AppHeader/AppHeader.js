1a24e1d7d3c40b3c7696cf7dbabfb3a305c297c4
stuart runyan <shrunyan@gmail.com>
2018-04-23 14:34:59 -0700

    WIP: accounts dropdown

--- src/shell/components/AppHeader/AppHeader.js
+++ src/shell/components/AppHeader/AppHeader.js
@@ -19,30 +19,51 @@ export default class AppHeader extends Component {
         <nav className={styles.GlobalNav}>
           <NavLink to="/properties">Web Properties</NavLink>
         </nav>
-        <nav className={styles.HelpNav}>
+        {/* <nav className={styles.HelpNav}>
           <a href="https://developer.zesty.io" target="_blank">
             <i className={cx(styles.icon, "fa fa-question-circle")} aria-hidden="true"></i>
           </a>
-        </nav>
-        <nav className={cx(styles.UserNav, styles[this.state.userNavOpen])} onClick={this.showUserNav}>
+        </nav> */}
+        <nav
+          className={cx(styles.UserNav, styles[this.state.userNavOpen])}
+          onClick={this.showUserNav}
+        >
+          {this.props.user.firstName} {this.props.user.lastName}
           <img
             className={styles.avatar}
             src={`https://www.gravatar.com/avatar/${
               this.props.user.emailHash
             }?d=mm&s=30`}
           />
-
           <ul className={styles.UserMenu}>
             <li className={styles.user}>
-              <span className={styles.name}>{this.props.user.firstName} {this.props.user.lastName}</span>
+              <span className={styles.name}>
+                {this.props.user.firstName} {this.props.user.lastName}
+              </span>
               <span className={styles.email}>{this.props.user.email}</span>
             </li>
+
+            <hr />
+
             <li>
               <NavLink to="/settings/account">
                 <i className="fa fa-cog" aria-hidden="true" />
                 &nbsp;My Account
               </NavLink>
             </li>
+
+            <li>
+              <a href="https://developer.zesty.io" target="_blank">
+                <i
+                  className={cx(styles.icon, 'fa fa-question-circle')}
+                  aria-hidden="true"
+                />{' '}
+                Support
+              </a>
+            </li>
+
+            <hr />
+
             <li
               className={styles.logout}
               title="Logout"
@@ -56,7 +77,7 @@ export default class AppHeader extends Component {
       </header>
     )
   }
-  showUserNav = (evt) => {
+  showUserNav = evt => {
     this.setState({
       userNavOpen: this.state.userNavOpen === 'show' ? '' : 'show'
     })
