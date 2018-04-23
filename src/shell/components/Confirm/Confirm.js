fa0633d9180678a5b5e2352904b9bd415990dc0f
Grant Glidewell <grant.glidewell@zesty.io>
2018-04-23 16:02:31 -0700

    added: confim component functional

--- src/shell/components/Confirm/Confirm.js
+++ src/shell/components/Confirm/Confirm.js
@@ -6,18 +6,22 @@ import styles from './Confirm.less'
 const Confirm = props => {
   return (
     (props.isOpen && (
-      <section className={styles.Confirm}>
-        <h1>{props.prompt}</h1>
-        <ButtonGroup>
-          <Button
-            onClick={() => props.dispatch({ type: 'REMOVE_CONFIRM' })}
-            text="Continue"
-          />
-          <Button
-            onClick={() => props.dispatch({ type: 'REMOVE_CONFIRM' })}
-            text="Cancel"
-          />
-        </ButtonGroup>
+      <section className={styles.confirmWrapper}>
+        <section className={styles.Confirm}>
+          <h1>{props.prompt}</h1>
+        <footer>
+          <ButtonGroup className={styles.buttons}>
+            <Button
+              onClick={() => props.dispatch({ type: 'REMOVE_CONFIRM_TRUE' })}
+              text="Continue"
+            />
+            <Button
+              onClick={() => props.dispatch({ type: 'REMOVE_CONFIRM_FALSE' })}
+              text="Cancel"
+            />
+          </ButtonGroup>
+        </footer>
+        </section>
       </section>
     )) ||
     null
--- src/shell/store/confirm.js
+++ src/shell/store/confirm.js
@@ -1,28 +1,24 @@
-const initialState = { isOpen: false };
+const initialState = { isOpen: false }
 
 export function confirm(state = initialState, action) {
   switch (action.type) {
-    case "NEW_CONFIRM":
-      return { ...state, isOpen: true, ...action };
-    case "REMOVE_CONFIRM":
-      return { isOpen: false };
+    case 'NEW_CONFIRM':
+      return { isOpen: true, ...action }
+    case 'REMOVE_CONFIRM_TRUE':
+      return { isOpen: false, result: true }
+    case 'REMOVE_CONFIRM_FALSE':
+      return { isOpen: false, result: false }
     default:
-      return state;
+      return state
   }
 }
 
-export function openConfirm(data) {
-  if(!data.prompt){
+export function newconfirm(data) {
+  if (!data.prompt) {
     console.error('cannot confirm without a prompt')
   }
   return {
-    type: "NEW_CONFIRM",
+    type: 'NEW_CONFIRM',
     data
-  };
-}
-
-export function closeConfirm() {
-  return {
-    type: "REMOVE_CONFIRM"
-  };
+  }
 }
