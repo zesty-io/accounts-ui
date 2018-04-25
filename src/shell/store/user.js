68cb3db204abb2a31eb48fa03990fba72ce49e46
stuart runyan <shrunyan@gmail.com>
2018-04-24 17:07:29 -0700

    fixed: code styling

--- src/shell/store/user.js
+++ src/shell/store/user.js
@@ -1,145 +1,144 @@
-import config from "../config";
-import { request } from "../../util/request";
-import { notify } from "../store/notifications";
+import config from '../config'
+import { request } from '../../util/request'
+import { notify } from '../store/notifications'
 
 export function user(state = {}, action) {
   switch (action.type) {
-    case "FETCHING_USER":
+    case 'FETCHING_USER':
       // TODO show loading state?
-      return state;
+      return state
 
-    case "FETCH_USER_SUCCESS":
+    case 'FETCH_USER_SUCCESS':
       return {
         ...state,
         ...action.user
-      };
-    case "FETCH_AUTH_SUCCESS":
+      }
+    case 'FETCH_AUTH_SUCCESS':
       return {
         ...state,
         ZUID: action.ZUID
-      };
+      }
 
-    case "FETCH_VERIFY_SUCCESS":
+    case 'FETCH_VERIFY_SUCCESS':
       return {
         ...state,
         ZUID: action.ZUID
-      };
+      }
 
-    case "FETCH_USER_ERROR":
+    case 'FETCH_USER_ERROR':
       // TODO handle failure
-      return state;
+      return state
 
-    case "UPDATE_USER_PROFILE":
-      return { ...state, ...action.payload };
+    case 'UPDATE_USER_PROFILE':
+      return { ...state, ...action.payload }
 
-    case "SAVING_PROFILE":
-      return state;
+    case 'SAVING_PROFILE':
+      return state
 
-    case "SAVING_PROFILE_ERROR":
-      return state;
+    case 'SAVING_PROFILE_ERROR':
+      return state
 
-    case "SAVING_PROFILE_SUCCESS":
-      return state;
+    case 'SAVING_PROFILE_SUCCESS':
+      return state
 
-    case "SAVING_PASSWORD":
-      return state;
+    case 'SAVING_PASSWORD':
+      return state
 
-    case "SAVING_PASSWORD_ERROR":
-      return state;
+    case 'SAVING_PASSWORD_ERROR':
+      return state
 
-    case "SAVING_PASSWORD_SUCCESS":
-      return state;
+    case 'SAVING_PASSWORD_SUCCESS':
+      return state
 
-    case "ADDING_EMAIL":
-      return state;
+    case 'ADDING_EMAIL':
+      return state
 
-    case "ADD_EMAIL_SUCCESS":
-      return state;
+    case 'ADD_EMAIL_SUCCESS':
+      return state
 
-    case "ADD_EMAIL_FAILURE":
-      return state;
+    case 'ADD_EMAIL_FAILURE':
+      return state
 
-    case "USER_INVITED":
-      return { ...state, ...action.invite };
+    case 'USER_INVITED':
+      return { ...state, ...action.invite }
 
     default:
-      return state;
+      return state
   }
 }
 
 export function fetchUser(ZUID) {
   return dispatch => {
     dispatch({
-      type: "FETCHING_USER"
-    });
+      type: 'FETCHING_USER'
+    })
     request(`${config.API_ACCOUNTS}/users/${ZUID}`)
       .then(user => {
         dispatch({
-          type: "FETCH_USER_SUCCESS",
+          type: 'FETCH_USER_SUCCESS',
           user: user.data
-        });
+        })
       })
       .catch(err => {
-        console.table(err);
+        console.table(err)
         dispatch({
-          type: "FETCH_USER_ERROR",
+          type: 'FETCH_USER_ERROR',
           err
-        });
-      });
-  };
+        })
+      })
+  }
 }
 
 export function update2fa(add, payload) {
-  if(add){
+  if (add) {
     // start process of adding 2fa
     // payload will have user info
     // have to get authy user ID from somehwere
-  }
-  else {
+  } else {
     // call db to remove 2fa and do whatever cleanup is also required
     // PUT update user     "authyEnabled": "false"
     return (dispatch, getState) => {
       dispatch({
-        type: "SAVING_PROFILE"
-      });
-      const userZUID = getState().user.ZUID;
+        type: 'SAVING_PROFILE'
+      })
+      const userZUID = getState().user.ZUID
       return request(`${config.API_ACCOUNTS}/users/${userZUID}`, {
-        method: "PUT",
+        method: 'PUT',
         json: true,
         body: {
           authyEnabled: false
         }
       })
         .then(data => {
-          dispatch({ type: "SAVING_PROFILE_SUCCESS" });
-          return data;
+          dispatch({ type: 'SAVING_PROFILE_SUCCESS' })
+          return data
         })
         .catch(err => {
-          console.table(err);
-          dispatch({ type: "SAVING_PROFILE_ERROR" });
-          throw err;
-        });
-    };
+          console.table(err)
+          dispatch({ type: 'SAVING_PROFILE_ERROR' })
+          throw err
+        })
+    }
   }
 }
 
 export function updateProfile(payload) {
   return {
-    type: "UPDATE_USER_PROFILE",
+    type: 'UPDATE_USER_PROFILE',
     payload
-  };
+  }
 }
 
 export function saveProfile() {
   return (dispatch, getState) => {
-    let { settings } = getState();
+    let { settings } = getState()
     dispatch({
-      type: "SAVING_PROFILE"
-    });
-    const userZUID = getState().user.ZUID;
-    const user = getState().user;
+      type: 'SAVING_PROFILE'
+    })
+    const userZUID = getState().user.ZUID
+    const user = getState().user
     return request(`${config.API_ACCOUNTS}/users/${userZUID}`, {
-      method: "PUT",
+      method: 'PUT',
       json: true,
       body: {
         firstName: user.firstName,
@@ -147,63 +146,68 @@ export function saveProfile() {
       }
     })
       .then(data => {
-        dispatch({ type: "SAVING_PROFILE_SUCCESS" });
-        return data;
+        dispatch({ type: 'SAVING_PROFILE_SUCCESS' })
+        return data
       })
       .catch(err => {
-        console.table(err);
-        dispatch({ type: "SAVING_PROFILE_ERROR" });
-        throw err;
-      });
-  };
+        console.table(err)
+        dispatch({ type: 'SAVING_PROFILE_ERROR' })
+        throw err
+      })
+  }
 }
 
 export function updatePassword(oldPassword, password) {
   return (dispatch, getState) => {
     dispatch({
-      type: "SAVING_PASSWORD"
-    });
+      type: 'SAVING_PASSWORD'
+    })
     const user = getState().user
     return request(`${config.API_AUTH}/login`, {
       body: {
         email: user.email,
         password: oldPassword
       }
-    }).then(data => {
-      return request(`${config.API_ACCOUNTS}/users/${user.ZUID}?updatePassword=true`, {
-        method: "PUT",
-        json: true,
-        body: {
-          password
-        }
-      }).then(data => {
-          dispatch({ type: "SAVING_PASSWORD_SUCCESS" });
-          return data;
+    })
+      .then(data => {
+        return request(
+          `${config.API_ACCOUNTS}/users/${user.ZUID}?updatePassword=true`,
+          {
+            method: 'PUT',
+            json: true,
+            body: {
+              password
+            }
+          }
+        ).then(data => {
+          dispatch({ type: 'SAVING_PASSWORD_SUCCESS' })
+          return data
         })
-    }).catch(err => {
-        console.table(err);
-        dispatch({ type: "SAVING_PASSWORD_ERROR" });
-        throw err;
-      });
-  };
+      })
+      .catch(err => {
+        console.table(err)
+        dispatch({ type: 'SAVING_PASSWORD_ERROR' })
+        throw err
+      })
+  }
 }
 
 export function addEmail() {
   return (dispatch, getState) => {
     dispatch({
-      type: "ADDING_EMAIL"
-    });
-    const userZUID = getState().user.ZUID;
+      type: 'ADDING_EMAIL'
+    })
+    const userZUID = getState().user.ZUID
     const unverifiedEmails =
       getState().user.unverifiedEmails !== null &&
-      getState().user.unverifiedEmails !== ""
+      getState().user.unverifiedEmails !== ''
         ? getState()
-            .user.unverifiedEmails.split(",")
+            .user.unverifiedEmails.split(',')
             .concat(getState().user.newEmail)
-            .join(",")
-        : getState().user.newEmail;
+            .join(',')
+        : getState().user.newEmail
     return request(`${config.API_ACCOUNTS}/users/${userZUID}`, {
-      method: "PUT",
+      method: 'PUT',
       json: true,
       body: {
         unverifiedEmails
@@ -212,22 +216,22 @@ export function addEmail() {
       .then(data => {
         dispatch(
           notify({
-            message: "Email added",
-            type: "success"
+            message: 'Email added',
+            type: 'success'
           })
-        );
-        dispatch(fetchUser(userZUID));
-        dispatch({ type: "ADD_EMAIL_SUCCESS" });
-        return data;
+        )
+        dispatch(fetchUser(userZUID))
+        dispatch({ type: 'ADD_EMAIL_SUCCESS' })
+        return data
       })
       .catch(error => {
         dispatch(
           notify({
             message: `Problem adding email: ${error}`,
-            type: "error"
+            type: 'error'
           })
-        );
-        return dispatch({ type: "ADD_EMAIL_FAILURE" });
-      });
-  };
+        )
+        return dispatch({ type: 'ADD_EMAIL_FAILURE' })
+      })
+  }
 }
