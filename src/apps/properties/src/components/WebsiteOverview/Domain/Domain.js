8bb518bd39b55f99455d5ba57984247073480f2c
Grant Glidewell <grant.glidewell@zesty.io>
2018-04-25 15:58:11 -0700

    wip: domain, changed: form affordance

--- src/apps/properties/src/components/WebsiteOverview/Domain/Domain.js
+++ src/apps/properties/src/components/WebsiteOverview/Domain/Domain.js
@@ -1,51 +1,62 @@
-import React, { Component } from "react";
-import { connect } from "react-redux";
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-import { updateDomain } from "../../../store/sitesDomain";
-import { notify } from "../../../../../../shell/store/notifications";
+import { updateDomain } from '../../../store/sitesDomain'
+import { notify } from '../../../../../../shell/store/notifications'
 
-import styles from "./Domain.less";
+import styles from './Domain.less'
 
 class Domain extends Component {
-  onChange = e => {
-    // use updateDomain function to handle vanity zesty or custom domains
-    this.props.dispatch({
-      type: "CHANGE_DOMAIN",
-      domainType: e.target.id,
-      zuid: this.props.site.zuid
-    });
-  };
+  constructor(props) {
+    super()
+    this.state = {
+      inputName: false,
+      name: ''
+    }
+  }
+  onChange = evt => {
+    evt.preventDefault()
+    this.setState({ [evt.target.name]: evt.target.value })
+  }
+  onSelect = evt => {
+    evt.preventDefault()
+
+  }
+  onUpdate = () => {
+    this.props.dispatch(updateDomain(this.props.siteZUID, this.state.name))
+  }
 
   render() {
     return (
-      <div className={styles.Domain}>
-        <label>Custom Domain</label>
-        <Input
-          type="radio"
-          name="domain"
-          id="custom"
-          onChange={this.onChange}
-          checked={this.props.site.domainSelect === "custom" ? "checked" : ""}
-        />
-        <label>Vanity Domain</label>
-        <Input
-          type="radio"
-          name="domain"
-          id="vanity"
-          checked={this.props.site.domainSelect === "vanity" ? "checked" : ""}
-          onChange={this.onChange}
-        />
-        <label>No Domain</label>
-        <Input
-          type="radio"
-          name="domain"
-          id="no"
-          onChange={this.onChange}
-          checked={this.props.site.domain === null ? "checked" : ""}
-        />
-      </div>
-    );
+      <React.Fragment>
+        <div
+          className={
+            this.state.inputName ? styles.nameInput : styles.invisible
+          }>
+          <Input type="text" name="name" onChange={this.handleName} />
+        </div>
+
+        <div className={styles.Domain}>
+          <label>Custom Domain</label>
+          <Input
+            type="radio"
+            name="domain"
+            id="custom"
+            onChange={this.onChange}
+            checked={this.props.site.domainSelect === 'custom' ? 'checked' : ''}
+          />
+          <label>Zesty.io Vanity URL</label>
+          <Input
+            type="radio"
+            name="domain"
+            id="vanity"
+            checked={this.props.site.domainSelect === 'vanity' ? 'checked' : ''}
+            onChange={this.onChange}
+          />
+        </div>
+      </React.Fragment>
+    )
   }
 }
 
-export default connect(state => state)(Domain);
+export default connect(state => state)(Domain)
