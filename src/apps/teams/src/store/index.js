b619badb70026ea27dc519a87676274d8f48061b
Grant Glidewell <grant.glidewell@zesty.io>
2018-05-14 09:24:19 -0700

    added: annotation for temp fake data being used for teams

--- src/apps/teams/src/store/index.js
+++ src/apps/teams/src/store/index.js
@@ -15,43 +15,7 @@ export function teams(state = {}, action) {
 // CRUD teams here
 
 export const fetchTeams = userZUID => {
-  return dispatch => {
-    dispatch({
-      type: 'FETCH_TEAMS_SUCCESS',
-      data: [
-        {
-          ZUID: 'dev-team-zuid',
-          name: 'Dev Team',
-          members: [
-            { name: 'emily', ZUID: 'userAZUID', email: 'aras@envoy.com' },
-            { name: 'sadfhadfhara', ZUID: 'userZUID', email: 'aras@ensdfgvoy.com' },
-            { name: 'sarsdhsdfa', ZUID: 'userZUIdsfgD', email: 'aras@envsdfgoy.com' },
-            { name: 'don', ZUID: 'useraASZUID', email: 'e@mail.com' }
-          ]
-        },
-        {
-          ZUID: 'seo-team-zuid',
-          name: 'SEO Team',
-          members: [
-            { name: 'sardfhgdsfa', ZUID: 'userZUsdhsdfhID', email: 'aras@envoy.com' },
-            { name: 'sa544554ra', ZUID: 'userZasdfUID', email: 'aras@envoy.com' },
-            { name: 'charles', ZUID: 'userZUsdfhID', email: 'e@mail.com' }
-          ]
-        },
-        {
-          ZUID: 'content-team-zuid',
-          name: 'Content Team',
-          members: [
-            { name: 'adam', ZUID: 'userasdfZUID', email: 'aras@envoy.com' },
-            { name: 'linda', ZUID: 'usersadfZUID', email: 'aras@envoy.com' },
-            { name: 'steve', ZUID: 'userasdfZasdfUID', email: 'aras@envoy.com' },
-            { name: 'dave', ZUID: 'userZUsdfhsdID', email: 'aras@envoy.com' },
-            { name: 'brett', ZUID: 'userdsaZUID', email: 'e@mail.com' }
-          ]
-        }
-      ]
-    })
-  }
+  //TODO: endpoint for teams
   // return dispatch => {
   //   dispatch({ type: 'FETCHING_TEAMS' })
   //   return request(`${config.API_ACCOUNTS}/teams/${userZUID}`)
@@ -65,4 +29,61 @@ export const fetchTeams = userZUID => {
   //       return err
   //     })
   // }
+  return dispatch => {
+    dispatch({
+      type: 'FETCH_TEAMS_SUCCESS',
+      data: [
+        {
+          ZUID: 'dev-team-zuid',
+          name: 'Dev Team',
+          members: [
+            { name: 'emily', ZUID: 'userAZUID', email: 'aras@envoy.com' },
+            {
+              name: 'sadfhadfhara',
+              ZUID: 'userZUID',
+              email: 'aras@ensdfgvoy.com'
+            },
+            {
+              name: 'sarsdhsdfa',
+              ZUID: 'userZUIdsfgD',
+              email: 'aras@envsdfgoy.com'
+            },
+            { name: 'don', ZUID: 'useraASZUID', email: 'e@mail.com' }
+          ]
+        },
+        {
+          ZUID: 'seo-team-zuid',
+          name: 'SEO Team',
+          members: [
+            {
+              name: 'sardfhgdsfa',
+              ZUID: 'userZUsdhsdfhID',
+              email: 'aras@envoy.com'
+            },
+            {
+              name: 'sa544554ra',
+              ZUID: 'userZasdfUID',
+              email: 'aras@envoy.com'
+            },
+            { name: 'charles', ZUID: 'userZUsdfhID', email: 'e@mail.com' }
+          ]
+        },
+        {
+          ZUID: 'content-team-zuid',
+          name: 'Content Team',
+          members: [
+            { name: 'adam', ZUID: 'userasdfZUID', email: 'aras@envoy.com' },
+            { name: 'linda', ZUID: 'usersadfZUID', email: 'aras@envoy.com' },
+            {
+              name: 'steve',
+              ZUID: 'userasdfZasdfUID',
+              email: 'aras@envoy.com'
+            },
+            { name: 'dave', ZUID: 'userZUsdfhsdID', email: 'aras@envoy.com' },
+            { name: 'brett', ZUID: 'userdsaZUID', email: 'e@mail.com' }
+          ]
+        }
+      ]
+    })
+  }
 }
