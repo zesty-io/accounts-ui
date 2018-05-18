4f06caf410fa9a8949145a7512300827580f0ec1
stuart runyan <shrunyan@gmail.com>
2018-05-17 17:56:29 -0700

    clean up website invite card styling

--- src/apps/properties/src/components/WebsiteInvite/WebsiteInvite.js
+++ src/apps/properties/src/components/WebsiteInvite/WebsiteInvite.js
@@ -3,52 +3,46 @@ import { connect } from 'react-redux'
 import { Link, withRouter } from 'react-router-dom'
 
 import config from '../../../../../shell/config'
+
+// NOTE: Fetch Sites seems unecessary. Should be able to fetch the individual site
 import { fetchSites, acceptInvite, declineInvite } from '../../store/sites'
 import { notify } from '../../../../../shell/store/notifications'
 
 import cx from 'classnames'
 import styles from './WebsiteInvite.less'
 
-import { Line } from 'react-chartjs-2'
+// import { Line } from 'react-chartjs-2'
 
 class WebsiteInvite extends Component {
   constructor(props) {
     super()
     this.state = {
-      accepted: false,
-      declined: false
+      submitted: false
     }
   }
 
   render() {
     return (
-      <article className={styles.WebsiteInvite}>
-        <header>
-          <h1 className={styles.name}>{this.props.site.name}</h1>
-          {this.props.site.domain ? (
-            <Url target="_blank" href={`http://${this.props.site.domain}`}>
-              <i className="fa fa-globe" aria-hidden="true" />&nbsp;{
-                this.props.site.domain
-              }
-            </Url>
-          ) : null}
-        </header>
-        <main className={styles.WebsiteManage}>
+      <Card className={styles.WebsiteInvite}>
+        <CardHeader>
+          <h1 className={styles.Name}>{this.props.site.name}</h1>
+        </CardHeader>
+        <CardContent>
           <Url
-            className={styles.preview}
+            className={styles.Preview}
             target="_blank"
             title={`Preview  ${this.props.site.name}`}
             href={`https://${this.props.site.RandomHashID}.preview.zesty.io`}
           >
             <i className={cx(styles.icon, 'fa fa-globe')} aria-hidden="true" />
           </Url>
-        </main>
-        <footer>
-          <ButtonGroup>
+        </CardContent>
+        <CardFooter>
+          <ButtonGroup className={styles.Actions}>
             <Button
               className={styles.invite}
               onClick={this.handleAccept}
-              disabled={this.state.accepted}
+              disabled={this.state.submitted}
             >
               <i className="fa fa-check-circle-o" aria-hidden="true" />
               Accept Invite
@@ -56,18 +50,18 @@ class WebsiteInvite extends Component {
             <Button
               type="cancel"
               onClick={this.handleDecline}
-              disabled={this.state.declined}
+              disabled={this.state.submitted}
             >
               <i className="fa fa-ban" aria-hidden="true" />
               Decline
             </Button>
           </ButtonGroup>
-        </footer>
-      </article>
+        </CardFooter>
+      </Card>
     )
   }
   handleAccept = evt => {
-    this.setState({ accepted: true })
+    this.setState({ submitted: true })
     // post accepted invite data THEN route to the overview when the user has permissions
     this.props.dispatch(acceptInvite(this.props.site.inviteZUID)).then(data => {
       this.props.dispatch(fetchSites()).then(data => {
@@ -91,7 +85,7 @@ class WebsiteInvite extends Component {
     })
   }
   handleDecline = evt => {
-    this.setState({ declined: true })
+    this.setState({ submitted: true })
     this.props
       .dispatch(declineInvite(this.props.site.inviteZUID))
       .then(data => {
