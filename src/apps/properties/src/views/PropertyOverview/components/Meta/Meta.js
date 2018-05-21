e7c2da736e14b1803f6a18846531d530cc10cb3a
Grant Glidewell <grant.glidewell@zesty.io>
2018-05-21 15:43:37 -0700

    added: metadata passd to meta component

--- src/apps/properties/src/views/PropertyOverview/components/Meta/Meta.js
+++ src/apps/properties/src/views/PropertyOverview/components/Meta/Meta.js
@@ -2,8 +2,17 @@ import { Component } from 'react'
 
 import styles from './Meta.less'
 
+const formatDate = date => {
+  if (!date) {
+    return ''
+  }
+  const newDate = new Date(date)
+  return `${newDate.getMonth() +
+    1}-${newDate.getDate()}-${newDate.getFullYear()}`
+}
 export default class Meta extends Component {
   render() {
+    console.log('props in meta', this.props)
     return (
       <Card>
         <CardHeader>
@@ -13,16 +22,19 @@ export default class Meta extends Component {
           </h2>
         </CardHeader>
         <CardContent>
-          <div className={styles.Roles}>
-            {/* <Divider /> */}
-            <div className={styles.currentRoles}>
-              <header>
-                <h3>Zuid</h3>
-              </header>
-              <main>
-                <h3>{this.props.siteZUID}</h3>
-              </main>
-            </div>
+          <div className={styles.Meta}>
+            <header>
+              <h3>Zuid</h3>
+              <h3>Created</h3>
+              <h3>Updated</h3>
+              <h3>Hash Id</h3>
+            </header>
+            <main>
+              <h3>{this.props.site.ZUID}</h3>
+              <h3>{formatDate(this.props.site.createdAt)}</h3>
+              <h3>{formatDate(this.props.site.updatedAt)}</h3>
+              <h3>{this.props.site.randomHashID}</h3>
+            </main>
           </div>
         </CardContent>
       </Card>
