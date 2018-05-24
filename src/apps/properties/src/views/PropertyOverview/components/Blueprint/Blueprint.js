73d73bd9633c407a272b22d81eb93b8ef983cecc
stuart runyan <shrunyan@gmail.com>
2018-05-24 11:26:16 -0700

    added: height/width controls for WithLoader component

--- src/apps/properties/src/views/PropertyOverview/components/Blueprint/Blueprint.js
+++ src/apps/properties/src/views/PropertyOverview/components/Blueprint/Blueprint.js
@@ -1,39 +1,41 @@
 import styles from './Blueprint.less'
 
 export default function Blueprint(props) {
+  console.log('Blueprint: ', props)
   return (
-    <React.Fragment>
-      {!props.loadingBlueprint ? (
-        <Card className={styles.Blueprint}>
-          <CardHeader>
-            <h2>
-              <i className="fa fa-file-code-o" aria-hidden="true" />
-              &nbsp;Blueprint
-            </h2>
-            {props.isAdmin ? (
-              <AppLink type="cancel" to={`${props.match.url}/blueprint`}>
-                <i className="fa fa-file-code-o" aria-hidden="true" />
-                &nbsp;Change Blueprint
-              </AppLink>
-            ) : null}
-          </CardHeader>
-          <CardContent>
-            <h2 className={styles.name}>{props.blueprint.name}</h2>
-            <img src={props.blueprint.coverImage} alt="" />
-            <p>{props.blueprint.description}</p>
+    <Card className={styles.Blueprint}>
+      <CardHeader>
+        <h2>
+          <i className="fa fa-file-code-o" aria-hidden="true" />
+          &nbsp;Blueprint
+        </h2>
+        {props.isAdmin ? (
+          <AppLink type="cancel" to={`${props.match.url}/blueprint`}>
+            <i className="fa fa-file-code-o" aria-hidden="true" />
+            &nbsp;Change Blueprint
+          </AppLink>
+        ) : null}
+      </CardHeader>
+      <CardContent>
+        <WithLoader
+          condition={!props.loadingBlueprint}
+          message="Loading Instance Blueprint"
+          height="200px"
+          width="100%"
+        >
+          <h2 className={styles.name}>{props.blueprint.name}</h2>
+          <img src={props.blueprint.coverImage} alt="" />
+          <p>{props.blueprint.description}</p>
 
-            {props.blueprint.githubURL ? (
-              <Url href={props.blueprint.githubURL} target="_blank">
-                <i className="fa fa-github" aria-hidden="true" />
-                View On Github
-              </Url>
-            ) : null}
-          </CardContent>
-        </Card>
-      ) : (
-        <Loader />
-      )}
-    </React.Fragment>
+          {props.blueprint.githubURL ? (
+            <Url href={props.blueprint.githubURL} target="_blank">
+              <i className="fa fa-github" aria-hidden="true" />
+              View On Github
+            </Url>
+          ) : null}
+        </WithLoader>
+      </CardContent>
+    </Card>
   )
   const handleChangeBlueprint = evt => {
     console.log(evt)
