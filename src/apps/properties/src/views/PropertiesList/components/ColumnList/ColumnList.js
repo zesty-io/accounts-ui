d0f15a3f5c2716d3f312fc2960f6f081253c7414
stuart runyan <shrunyan@gmail.com>
2018-06-04 10:47:49 -0700

    added: section titles in list view

--- src/apps/properties/src/views/PropertiesList/components/ColumnList/ColumnList.js
+++ src/apps/properties/src/views/PropertiesList/components/ColumnList/ColumnList.js
@@ -10,47 +10,106 @@ export default function ColumnList(props) {
   return (
     <section className={styles.ColumnList}>
       <nav className={styles.List}>
-        {props.sitesInvited.map(site => {
-          return (
-            <span key={site.ZUID} className={cx(styles.row, styles.invite)}>
-              <AppLink className={styles.row} to={`/instances/${site.ZUID}`}>
-                {site.name}
-              </AppLink>
-              <Button>
-                <i className="fa fa-check" aria-hidden="true" />
-              </Button>
-              <Button type="cancel">
-                <i className="fa fa-ban" aria-hidden="true" />
-              </Button>
-            </span>
-          )
-        })}
-        <Divider />
-        {props.sitesFiltered.map(site => {
-          return (
-            <span key={site.ZUID} className={styles.row}>
-              <AppLink to={`/instances/${site.ZUID}`}>{site.name}</AppLink>
-              <Url
-                target="_blank"
-                title={`Open instance preview: ${site.name}`}
-                href={`${CONFIG.PREVIEW_URL_PROTOCOL}${site.randomHashID}${
-                  CONFIG.PREVIEW_URL
-                }`}
-              >
-                <i className={'fa fa-eye'} aria-hidden="true" />
-              </Url>
-              <Url
-                target="_blank"
-                title="Open instance manager"
-                href={`${CONFIG.MANAGER_URL_PROTOCOL}${site.randomHashID}${
-                  CONFIG.MANAGER_URL
-                }`}
-              >
-                <i className="fa fa-external-link-square" aria-hidden="true" />
-              </Url>
-            </span>
-          )
-        })}
+        {props.sitesInvited.length ? (
+          <React.Fragment>
+            <h2 className={styles.SectionTitle}>
+              <i className="fa fa-envelope-o" aria-hidden="true" />
+              &nbsp;Invites
+            </h2>
+            {props.sitesInvited.map(site => {
+              return (
+                <span key={site.ZUID} className={cx(styles.row, styles.invite)}>
+                  <AppLink
+                    className={styles.row}
+                    to={`/instances/${site.ZUID}`}
+                  >
+                    {site.name}
+                  </AppLink>
+                  <Button>
+                    <i className="fa fa-check" aria-hidden="true" />
+                  </Button>
+                  <Button type="cancel">
+                    <i className="fa fa-ban" aria-hidden="true" />
+                  </Button>
+                </span>
+              )
+            })}
+          </React.Fragment>
+        ) : null}
+
+        {props.sitesFavorite.length ? (
+          <React.Fragment>
+            <h2 className={styles.SectionTitle}>
+              <i className="fa fa-star-o" aria-hidden="true" />
+              &nbsp;Favorites
+            </h2>
+            {props.sitesFavorite.map(site => {
+              return (
+                <span key={site.ZUID} className={styles.row}>
+                  <AppLink to={`/instances/${site.ZUID}`}>{site.name}</AppLink>
+                  <Url
+                    target="_blank"
+                    title={`Open instance preview: ${site.name}`}
+                    href={`${CONFIG.PREVIEW_URL_PROTOCOL}${site.randomHashID}${
+                      CONFIG.PREVIEW_URL
+                    }`}
+                  >
+                    <i className={'fa fa-eye'} aria-hidden="true" />
+                  </Url>
+                  <Url
+                    target="_blank"
+                    title="Open instance manager"
+                    href={`${CONFIG.MANAGER_URL_PROTOCOL}${site.randomHashID}${
+                      CONFIG.MANAGER_URL
+                    }`}
+                  >
+                    <i
+                      className="fa fa-external-link-square"
+                      aria-hidden="true"
+                    />
+                  </Url>
+                </span>
+              )
+            })}
+          </React.Fragment>
+        ) : null}
+
+        {props.sitesFiltered.length ? (
+          <React.Fragment>
+            <h2 className={styles.SectionTitle}>
+              <i className="fa fa-th" aria-hidden="true" />
+              &nbsp;All Instances
+            </h2>
+            {props.sitesFiltered.map(site => {
+              return (
+                <span key={site.ZUID} className={styles.row}>
+                  <AppLink to={`/instances/${site.ZUID}`}>{site.name}</AppLink>
+                  <Url
+                    target="_blank"
+                    title={`Open instance preview: ${site.name}`}
+                    href={`${CONFIG.PREVIEW_URL_PROTOCOL}${site.randomHashID}${
+                      CONFIG.PREVIEW_URL
+                    }`}
+                  >
+                    <i className={'fa fa-eye'} aria-hidden="true" />
+                  </Url>
+                  <Url
+                    target="_blank"
+                    title="Open instance manager"
+                    href={`${CONFIG.MANAGER_URL_PROTOCOL}${site.randomHashID}${
+                      CONFIG.MANAGER_URL
+                    }`}
+                  >
+                    <i
+                      className="fa fa-external-link-square"
+                      aria-hidden="true"
+                    />
+                  </Url>
+                </span>
+              )
+            })}
+          </React.Fragment>
+        ) : null}
       </nav>
       <main className={styles.Overview}>
         <Switch>
