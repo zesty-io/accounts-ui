c4ddfd80908ab49c052aca5ed31929c0d95c3ce0
Grant Glidewell <grant.glidewell@zesty.io>
2018-06-07 11:11:54 -0700

    added: create team forms and state interacitons

--- src/apps/teams/src/components/CreateTeam/CreateTeam.js
+++ src/apps/teams/src/components/CreateTeam/CreateTeam.js
@@ -2,32 +2,62 @@ import React, { Component } from 'react'
 
 import styles from './create.less'
 class CreateTeam extends Component {
+  state = {
+    name: '',
+    invitees: ['', '', ''],
+    submitted: false
+  }
   render() {
     return (
       <Card className={styles.Card}>
         <CardHeader>Create Team</CardHeader>
         <CardContent>
           <p>
-            Create a team to add multiple employees/users to gain acess to a
-            site altogether
+            Add multiple employees/users to a team and gain access to an
+            instance together
           </p>
           <label>name team</label>
-          <Input type="text" />
+          <Input
+            type="text"
+            value={this.state.name}
+            onChange={this.changeName}
+          />
           <label>invite members by email</label>
-          <Input type="text" />
-          <Input type="text" />
-          <Input type="text" /><br />
-          <i className="fa fa-plus">add member</i>
+          {this.state.invitees.map((email, i) => {
+            return (
+              <Input key={i} name={i} value={email} onChange={this.addInvite} />
+            )
+          })}
+          <br />
+          <span className={styles.addMember} onClick={this.addInvitee}>
+            <i className="fa fa-plus" />
+            <h4>add member</h4>
+          </span>
         </CardContent>
         <CardFooter>
           <Button
-            onClick={() => console.log('click', team.ZUID)}
+            disabled={this.state.submitted}
+            onClick={this.handleSubmit}
             text="Create Team"
           />
         </CardFooter>
       </Card>
     )
   }
+  addInvitee = () => {
+    const invitees = this.state.invitees
+    invitees.push('')
+    this.setState({ invitees })
+  }
+  addInvite = evt => {
+    const invitees = this.state.invitees
+    invitees.splice(Number(evt.target.name), 1, evt.target.value)
+    this.setState({ invitees })
+  }
+  changeName = evt => {
+    this.setState({ name: evt.target.value })
+  }
+  handleSubmit = evt => {}
 }
 
 export default CreateTeam
