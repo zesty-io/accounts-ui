f0f98869a5e9707fd52b478622d543a7f4a1e9a6
stuart runyan <shrunyan@gmail.com>
2018-06-07 14:50:20 -0700

    added: include access level for system roles(support legacy permissions)

--- src/shell/store/systemRoles.js
+++ src/shell/store/systemRoles.js
@@ -14,29 +14,35 @@ export function fetchSystemRoles() {
     dispatch({
       type: 'FETCH_SYSTEM_ROLES_SUCCESS',
       roles: {
+        '31-71cfc74-0wn3r': {
+          ZUID: '31-71cfc74-0wn3r',
+          name: 'Owner',
+          accessLevel: 0
+        },
         '31-71cfc74-4dm13': {
           ZUID: '31-71cfc74-4dm13',
-          name: 'Admin'
-        },
-        '31-71cfc74-c0ntr1b0t0r': {
-          ZUID: '31-71cfc74-c0ntr1b0t0r',
-          name: 'Contributor'
+          name: 'Admin',
+          accessLevel: 1
         },
         '31-71cfc74-d3v3l0p3r': {
           ZUID: '31-71cfc74-d3v3l0p3r',
-          name: 'Developer'
-        },
-        '31-71cfc74-0wn3r': {
-          ZUID: '31-71cfc74-0wn3r',
-          name: 'Owner'
-        },
-        '31-71cfc74-p0bl1shr': {
-          ZUID: '31-71cfc74-p0bl1shr',
-          name: 'Publisher'
+          name: 'Developer',
+          accessLevel: 2
         },
         '31-71cfc74-s30': {
           ZUID: '31-71cfc74-s30',
-          name: 'SEO'
+          name: 'SEO',
+          accessLevel: 3
+        },
+        '31-71cfc74-p0bl1shr': {
+          ZUID: '31-71cfc74-p0bl1shr',
+          name: 'Publisher',
+          accessLevel: 4
+        },
+        '31-71cfc74-c0ntr1b0t0r': {
+          ZUID: '31-71cfc74-c0ntr1b0t0r',
+          name: 'Contributor',
+          accessLevel: 5
         }
       }
     })
