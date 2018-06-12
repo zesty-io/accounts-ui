84e8f0ce6dace4468144ee7f545b268c54fa4a08
stuart runyan <shrunyan@gmail.com>
2018-06-11 18:10:26 -0700

    clean up support section

--- src/apps/support/src/components/SupportOptions/SupportOptions.js
+++ src/apps/support/src/components/SupportOptions/SupportOptions.js
@@ -1,82 +1,43 @@
-import React, { Component } from 'react'
-import { connect } from 'react-redux'
-import { withRouter } from 'react-router-dom'
-
-import { request } from '../../../../../util/request'
-
-
+import React, { PureComponent } from 'react'
+// import { connect } from 'react-redux'
+// import { request } from '../../../../../util/request'
 import styles from './support.less'
 
-class SupportOptions extends Component {
-  state = {
-    userInfo: {}
-  }
-
+export default class SupportOptions extends PureComponent {
   render() {
     return (
-      <div className={styles.Support}>
-        <h1 className={styles.SupportTitle}>Support</h1>
+      <section className={styles.Support}>
+        <header>
+          <h1 className={styles.SupportTitle}>Support</h1>
+        </header>
+        <main>
+          <a href="mailto://support@zesty.io">
+            <i className="fa fa-envelope-o fa-3x" />
+            <span>Contact Support</span>
+          </a>
+          <a href="http://learn.zesty.io" target="_blank">
+            <i className="fa fa-files-o fa-3x" />
+            <span>Help Docs</span>
+          </a>
+          <a href="https://developer.zesty.io/" target="_blank">
+            <i className="fa fa-code fa-3x" />
+            <span>Developer Docs</span>
+          </a>
 
-        <section>
-          <article>
-            <a href="mailto://support@zesty.io">
-              {
-                //TODO: where should this link go?
-              }
-              <i className="fa fa-envelope-o fa-3x" />
-              <p> Contact Support </p>
-            </a>
-          </article>
-
-          <article>
-            <a href="http://learn.zesty.io" target="_blank">
-              <i className="fa fa-files-o fa-3x" />
-              <p> Help Docs </p>
-            </a>
-          </article>
-
-          <article>
-            <a href="https://forum.zesty.io/" target="_blank">
-              <i className="fa fa-comments-o fa-3x" />
-              <p> Forums </p>
-            </a>
-          </article>
-        </section>
-        <h2>Developer Community Resources</h2>
-
-        <section>
-          <article>
-            <a href="https://developer.zesty.io/" target="_blank">
-              <i className="fa fa-code fa-3x" />
-              <p> Developer Docs </p>
-            </a>
-          </article>
-
-          <article>
-            <a href="http://chat.zesty.io" target="_blank">
-              <i className="fa fa-slack fa-3x" />
-              <p> Developer Chat </p>
-            </a>
-          </article>
-        </section>
-        <h2>Report A Bug</h2>
-
-        <section>
-          <article>
-            <a href="" onClick={this.reportBug}>
-              <i className="fa fa-bug fa-3x" />
-              <p> Bug Report </p>
-            </a>
-          </article>
-        </section>
-      </div>
+          <a href="http://chat.zesty.io" target="_blank">
+            <i className="fa fa-slack fa-3x" />
+            <span>Developer Chat</span>
+          </a>
+          {/* <a href="javascript:void(0)" onClick={this.reportBug}>
+            <i className="fa fa-bug fa-3x" />
+            <span>Bug Report</span>
+          </a> */}
+        </main>
+      </section>
     )
   }
-
-  reportBug = (evt) => {
+  reportBug = evt => {
     evt.preventDefault()
     return this.props.history.push(`support/bugreport`)
   }
 }
-
-export default withRouter(connect(state => state)(SupportOptions))
