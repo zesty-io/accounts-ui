cca55e0e4c9b3dae43aa745683cef6d11ea2f344
stuart runyan <shrunyan@gmail.com>
2018-06-15 15:47:46 -0700

    fixed: button component to allow conditional pass through of icons

--- src/core/Button/Button.js
+++ src/core/Button/Button.js
@@ -9,18 +9,21 @@ export default function Button(props) {
       className={cx(styles.button, props.className, styles[props.type])}
     >
       {props.text}
-      {React.Children.map(props.children, (child, i) => {
-        // If the first child is an element
-        // assume it's an icon
-        if (child.props && i === 0) {
-          return React.cloneElement(child, {
-            className: cx(styles.icon, child.props.className)
-          })
-        } else {
-          // probably just a text node
-          return child
+      {React.Children.map(
+        React.Children.toArray(props.children),
+        (child, i) => {
+          // If the first child is an element
+          // assume it's an icon
+          if (child.props && i === 0) {
+            return React.cloneElement(child, {
+              className: cx(styles.icon, child.props.className)
+            })
+          } else {
+            // probably just a text node
+            return child
+          }
         }
-      })}
+      )}
     </button>
   )
 }
