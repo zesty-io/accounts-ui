a61b29da08dbb4827c915b9c5cb7a16c0ddd9efb
Grant Glidewell <grant.glidewell@zesty.io>
2018-06-19 16:19:16 -0700

    added: bugsnag react component wrapping app

--- src/shell/index.js
+++ src/shell/index.js
@@ -1,9 +1,14 @@
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
 
 import { store } from './store'
 import { verifyAuth } from './store/auth'
+
+const bugsnagClient = bugsnag('7e50d87ea61932f9e3141420402f4eed')
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 
 import App from './views/App'
 
@@ -11,11 +16,13 @@ let unsubscribe = store.subscribe(() => {
   let state = store.getState()
   if (!state.auth.checking) {
     ReactDOM.render(
-      <Provider store={store}>
-        <BrowserRouter>
-          <Route component={App} />
-        </BrowserRouter>
-      </Provider>,
+      <ErrorBoundary>
+        <Provider store={store}>
+          <BrowserRouter>
+            <Route component={App} />
+          </BrowserRouter>
+        </Provider>
+      </ErrorBoundary>,
       document.getElementById('root')
     )
   }
