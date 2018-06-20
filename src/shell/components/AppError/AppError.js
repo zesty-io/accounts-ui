c724dd89cc84b30ca6cd8f5a2493b5bb49d9d500
Grant Glidewell <grant.glidewell@zesty.io>
2018-06-20 09:00:34 -0700

    moved: bugsnag component into app error

--- src/shell/components/AppError/AppError.js
+++ src/shell/components/AppError/AppError.js
@@ -1,6 +1,10 @@
 import { Component } from 'React'
 import styles from './AppError.less'
+import bugsnag from 'bugsnag-js'
+import createPlugin from 'bugsnag-react'
 
+const bugsnagClient = bugsnag('7e50d87ea61932f9e3141420402f4eed')
+const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 export default class AppError extends Component {
   constructor(props) {
     super(props)
@@ -25,23 +29,24 @@ export default class AppError extends Component {
   render() {
     if (this.state.hasError) {
       return (
-        <section className={styles.AppCrash}>
-          <h1>
-            <i className="fa fa-bug" aria-hidden="true" />&nbsp;We apologize but
-            something went wrong
-          </h1>
-          <h3>
-            Try reloading the application (CMD + R).&nbsp;
-            <Url
-              href={`mailto:support@zesty.io?subject=Accounts App Crash&body=REPLACE WITH EXTRA INFORMATION ---- ${
-                this.state.err
-              }`}
-              target="_blank"
-            >
-              Report to support@zesty.io
-            </Url>
-          </h3>
-        </section>
+        <ErrorBoundary>
+          <section className={styles.AppCrash}>
+            <h1>
+              <i className="fa fa-bug" aria-hidden="true" />&nbsp;We apologize
+              but something went wrong
+            </h1>
+            <h3>
+              Try reloading the application (CMD + R).&nbsp;
+              <Url
+                href={`mailto:support@zesty.io?subject=Accounts App Crash&body=REPLACE WITH EXTRA INFORMATION ---- ${
+                  this.state.err
+                }`}
+                target="_blank">
+                Report to support@zesty.io
+              </Url>
+            </h3>
+          </section>
+        </ErrorBoundary>
       )
     } else {
       return this.props.children
--- src/shell/index.js
+++ src/shell/index.js
@@ -1,14 +1,9 @@
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { BrowserRouter, Route } from 'react-router-dom'
-import bugsnag from 'bugsnag-js'
-import createPlugin from 'bugsnag-react'
 
 import { store } from './store'
 import { verifyAuth } from './store/auth'
-
-const bugsnagClient = bugsnag('7e50d87ea61932f9e3141420402f4eed')
-const ErrorBoundary = bugsnagClient.use(createPlugin(React))
 
 import App from './views/App'
 
@@ -16,13 +11,11 @@ let unsubscribe = store.subscribe(() => {
   let state = store.getState()
   if (!state.auth.checking) {
     ReactDOM.render(
-      <ErrorBoundary>
-        <Provider store={store}>
-          <BrowserRouter>
-            <Route component={App} />
-          </BrowserRouter>
-        </Provider>
-      </ErrorBoundary>,
+      <Provider store={store}>
+        <BrowserRouter>
+          <Route component={App} />
+        </BrowserRouter>
+      </Provider>,
       document.getElementById('root')
     )
   }
