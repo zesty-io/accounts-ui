5f3ea4174c204353a1641e28c002c101db117dc4
Grant Glidewell <grant.glidewell@zesty.io>
2018-06-21 12:15:10 -0700

    changed: search component to a stateless functional component

--- src/core/Search/Search.js
+++ src/core/Search/Search.js
@@ -2,39 +2,32 @@ import React from 'react'
 import styles from './Search.less'
 import cx from 'classnames'
 
-export default class Search extends React.Component {
-  state = {
-    term: ''
-  }
-  render() {
-    return (
-      <div className={cx(styles.search, this.props.className)}>
-        <Button className={styles.searchBtn} onClick={this.handleFilter}>
-          <i
-            className={cx(styles.searchIcon, 'fa fa-search')}
-            aria-hidden="true"
-          />
-        </Button>
-        <input
-          type="text"
-          className={styles.searchField}
-          placeholder={this.props.placeholder}
-          value={this.state.term}
-          onChange={evt => this.setState({ term: evt.target.value })}
-          onFocus={this.props.onFocus}
-          onKeyUp={this.props.onKeyUp}
-        />
-      </div>
-    )
-  }
-  handleFilter = evt => {
-    // Prevent button click from triggering
-    // a form submission if this exists inside
-    // of a form element parent
+export default function Search(props) {
+  function handleFilter(evt) {
     evt.preventDefault()
-    if (!evt.target.value) {
-      evt.target.value = this.state.term
-    }
-    this.props.onClick(evt)
+    // returns the target value of the input
+    evt.target.value = evt.target.term.value
+    props.onClick(evt)
   }
+  return (
+    <form
+      className={cx(styles.search, props.className)}
+      onSubmit={handleFilter}>
+      <Button className={styles.searchBtn} type="submit">
+        <i
+          className={cx(styles.searchIcon, 'fa fa-search')}
+          aria-hidden="true"
+        />
+      </Button>
+      <input
+        type="text"
+        name="term"
+        autoComplete="off"
+        className={styles.searchField}
+        placeholder={props.placeholder}
+        onFocus={props.onFocus}
+        onKeyUp={props.onKeyUp}
+      />
+    </form>
+  )
 }
