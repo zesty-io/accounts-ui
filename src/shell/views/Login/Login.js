8ad9a5facdffee19dec8beb596b3d2e7806a162e
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-06 16:25:33 -0700

    removed: unwanted links on login

--- src/shell/views/Login/Login.js
+++ src/shell/views/Login/Login.js
@@ -118,13 +118,6 @@ class Login extends Component {
               <AppLink to="/signup" tabIndex="5">
                 Create An Account
               </AppLink>
-              <h3>Additional Information</h3>
-              <p>
-                <Url href="https://zesty.io">https://zesty.io</Url>
-              </p>
-              <p>
-                <Url href="mailto:hello@zesty.io">hello@zesty.io</Url>
-              </p>
             </div>
           </main>
         </div>
