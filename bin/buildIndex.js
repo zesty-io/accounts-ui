6c9843b61bda217502aec660d6f1b0b3a918caad
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-09 14:07:42 -0700

    added polyfills for array includes, object keys

--- bin/buildIndex.js
+++ bin/buildIndex.js
@@ -130,7 +130,7 @@ module.exports = buildIndex = build => {
           </div>
         </div>
       </main>
-      <!-- Object.assign polyfill -->
+      <!-- polyfills for relic browsers -->
       <script>
       if (typeof Object.assign != 'function') {
         // Must be writable: true, enumerable: false, configurable: true
@@ -159,6 +159,96 @@ module.exports = buildIndex = build => {
           },
           writable: true,
           configurable: true
+        });
+      }
+      if (!Object.keys) {
+        Object.keys = (function() {
+          'use strict';
+          var hasOwnProperty = Object.prototype.hasOwnProperty,
+              hasDontEnumBug = !({ toString: null }).propertyIsEnumerable('toString'),
+              dontEnums = [
+                'toString',
+                'toLocaleString',
+                'valueOf',
+                'hasOwnProperty',
+                'isPrototypeOf',
+                'propertyIsEnumerable',
+                'constructor'
+              ],
+              dontEnumsLength = dontEnums.length;
+      
+          return function(obj) {
+            if (typeof obj !== 'function' && (typeof obj !== 'object' || obj === null)) {
+              throw new TypeError('Object.keys called on non-object');
+            }
+      
+            var result = [], prop, i;
+      
+            for (prop in obj) {
+              if (hasOwnProperty.call(obj, prop)) {
+                result.push(prop);
+              }
+            }
+      
+            if (hasDontEnumBug) {
+              for (i = 0; i < dontEnumsLength; i++) {
+                if (hasOwnProperty.call(obj, dontEnums[i])) {
+                  result.push(dontEnums[i]);
+                }
+              }
+            }
+            return result;
+          };
+        }());
+      }
+      if (!Array.prototype.includes) {
+        Object.defineProperty(Array.prototype, 'includes', {
+          value: function(searchElement, fromIndex) {
+      
+            if (this == null) {
+              throw new TypeError('"this" is null or not defined');
+            }
+      
+            // 1. Let O be ? ToObject(this value).
+            var o = Object(this);
+      
+            // 2. Let len be ? ToLength(? Get(O, "length")).
+            var len = o.length >>> 0;
+      
+            // 3. If len is 0, return false.
+            if (len === 0) {
+              return false;
+            }
+      
+            // 4. Let n be ? ToInteger(fromIndex).
+            //    (If fromIndex is undefined, this step produces the value 0.)
+            var n = fromIndex | 0;
+      
+            // 5. If n ≥ 0, then
+            //  a. Let k be n.
+            // 6. Else n < 0,
+            //  a. Let k be len + n.
+            //  b. If k < 0, let k be 0.
+            var k = Math.max(n >= 0 ? n : len - Math.abs(n), 0);
+      
+            function sameValueZero(x, y) {
+              return x === y || (typeof x === 'number' && typeof y === 'number' && isNaN(x) && isNaN(y));
+            }
+      
+            // 7. Repeat, while k < len
+            while (k < len) {
+              // a. Let elementK be the result of ? Get(O, ! ToString(k)).
+              // b. If SameValueZero(searchElement, elementK) is true, return true.
+              if (sameValueZero(o[k], searchElement)) {
+                return true;
+              }
+              // c. Increase k by 1. 
+              k++;
+            }
+      
+            // 8. Return false
+            return false;
+          }
         });
       }
       </script>
