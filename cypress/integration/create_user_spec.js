28ab1c611f21811654157eb904743c6b66bb85ae
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-09 12:55:26 -0700

    added: create user test

---
+++ cypress/integration/create_user_spec.js
@@ -0,0 +1,25 @@
+describe('User can create account', function() {
+  it('Creates a user', function() {
+    cy.visit(Cypress.env('ACCOUNTS_UI'))
+    cy.get('#root > section > div > main > div > a').click()
+    cy.get('#root > section > form > label:nth-child(2) > input').type(
+      `newUser${Date.now()}@email.com`
+    )
+    cy.get('#root > section > form > label:nth-child(3) > input').type(
+      'newUserName'
+    )
+    cy.get('#root > section > form > label:nth-child(4) > input').type(
+      'lastName'
+    )
+    cy.get('#root > section > form > label:nth-child(5) > input').type(
+      'asdASD123'
+    )
+    cy.get('#root > section > form > label:nth-child(6) > input').click()
+    cy.get('#root > section > form > button').click()
+    cy.wait(10000)
+    cy.get('#root > section > main > p').should(
+      'contain',
+      'We have sent a verification email to the email address you signed up with. This is done for account security to ensure you are the owner of the email address. Once you have verified your email return here to begin using Zesty.io'
+    )
+  })
+})
--- cypress/integration/login_spec.js
+++ cypress/integration/login_spec.js
@@ -15,22 +15,22 @@ describe('User gets failed login message', function() {
   })
 })
 
-// describe('User is able to login', function() {
-//   it('Logs in with good credentials', function() {
-//     cy.visit(Cypress.env("ACCOUNTS_UI"))
-//     cy.get('#root > section > div > main > form > label:nth-child(1) > input')
-//       .type(goodEmail)
-//       .should('have.value', goodEmail)
-//     cy.get('#root > section > div > main > form > label:nth-child(2) > input')
-//       .type(goodPassword)
-//       .should('have.value', goodPassword)
-//     cy.get('#root > section > div > main > form > button').click()
-//     // assert that the app loads
-//     cy.get('#root > section > div > main > form > p').should(
-//       'contain',
-//       'There was a problem logging you in'
-//     )
-//   })
-// })
+describe('User is able to login', function() {
+  it('Logs in with good credentials', function() {
+    cy.visit(Cypress.env('ACCOUNTS_UI'))
+    cy.get('#root > section > div > main > form > label:nth-child(1) > input')
+      .type(Cypress.env('goodEmail'))
+      .should('have.value', Cypress.env('goodEmail'))
+    cy.get('#root > section > div > main > form > label:nth-child(2) > input')
+      .type(Cypress.env('goodPassword'))
+      .should('have.value', Cypress.env('goodPassword'))
+    cy.get('#root > section > div > main > form > button').click()
+    // assert that the app loads
+    cy.get('#root > section > header > nav.UserNav.UserNav--IMX-X').should(
+      'contain',
+      'Glidewell'
+    )
+  })
+})
 
 // TODO: find a way to test 2FA users
