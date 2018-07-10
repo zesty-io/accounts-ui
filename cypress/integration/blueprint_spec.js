cf1d3f5cec00121def0d24fd80a2a333f99ca6dc
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-10 10:29:32 -0700

    blueprint creation test

--- cypress/integration/blueprint_spec.js
+++ cypress/integration/blueprint_spec.js
@@ -1,5 +1,6 @@
 describe('User is able to login', () => {
-  it('Logs in with good credentials', () => {
+  const timeStamp = Date.now()
+  it('Can create a blueprint', () => {
     cy.visit(Cypress.env('ACCOUNTS_UI'))
     cy.get('#root > section > div > main > form > label:nth-child(1) > input')
       .type(Cypress.env('validEmail'))
@@ -10,28 +11,23 @@ describe('User is able to login', () => {
     cy.get('#root > section > div > main > form > button').click()
     // assert that the app loads
     cy.get('#root > section > header > nav.UserNav').should('contain', 'test')
-  })
-
-  const timeStamp = Date.now()
-  it('Can create a blueprint', () => {
     cy.get('#blueprintsNavLink').click()
-    cy.get(
-      '#root > section > section.AppMain.AppMain > section > section > section > article > footer > div > a'
-    ).click()
+    cy.get('#createBlueprint').click()
 
-    cy.get('input')
-      .first()
-      .click()
-      .type(`Blueprint test ${timeStamp}`)
-      .should('contain', 'Blueprint test')
+    cy.get('#Blueprint > label:nth-child(2) > input').type(
+      `Blueprint test ${timeStamp}`
+    )
     cy.get('#Blueprint > label:nth-child(3) > input').type('fakeGithubURL')
     cy.get('#Blueprint > label:nth-child(4) > input').type('fakeCoverImageURL')
-    cy.get('#Blueprint > label:nth-child(5) > .Input').type(
-      'fakeShieldImageURL'
+    cy.get('#Blueprint > label:nth-child(5) > input').type('fakeShieldImageURL')
+    cy.get('#Blueprint > label:nth-child(6) > input').type('fakePreviewURL')
+    cy.get('#Blueprint > label:nth-child(7) > textarea').type(
+      'Short description'
     )
-    cy.get('#Blueprint > label:nth-child(6) > .Input').type('fakePreviewURL')
-    cy.get('#Blueprint > label:nth-child(7) > .Input').type('Short description')
-    cy.get('#Blueprint > label:nth-child(8) > .Input').type('Long description')
+    cy.get('#Blueprint > label:nth-child(8) > textarea').type(
+      'Long description'
+    )
+    cy.get('#Blueprint > div > button:nth-child(1)').click()
   })
 
   // it('can edit a blueprint')
--- cypress/integration/instance_spec.js
+++ cypress/integration/instance_spec.js
@@ -1,5 +1,6 @@
-describe('User is able to login', () => {
-  it('Logs in with good credentials', () => {
+describe('User can perform instance actions', () => {
+  const timeStamp = Date.now()
+  it('Cannot create an instance with no name', () => {
     cy.visit(Cypress.env('ACCOUNTS_UI'))
     cy.get('#root > section > div > main > form > label:nth-child(1) > input')
       .type(Cypress.env('validEmail'))
@@ -8,12 +9,6 @@ describe('User is able to login', () => {
       .type(Cypress.env('validPassword'))
       .should('have.value', Cypress.env('validPassword'))
     cy.get('#root > section > div > main > form > button').click()
-    // assert that the app loads
-    cy.get('#root > section > header > nav.UserNav').should('contain', 'test')
-  })
-
-  const timeStamp = Date.now()
-  it('Cannot create an instance with no name', () => {
     cy.get('#siteListWrapper > main > article > footer > a > button').click()
     cy.get(
       '#root > section > section > section > section > div > div > button'
@@ -25,12 +20,28 @@ describe('User is able to login', () => {
   })
 
   it('Can create an instance', () => {
+    cy.visit(Cypress.env('ACCOUNTS_UI'))
+    cy.get('#root > section > div > main > form > label:nth-child(1) > input')
+      .type(Cypress.env('validEmail'))
+      .should('have.value', Cypress.env('validEmail'))
+    cy.get('#root > section > div > main > form > label:nth-child(2) > input')
+      .type(Cypress.env('validPassword'))
+      .should('have.value', Cypress.env('validPassword'))
+    cy.get('#root > section > div > main > form > button').click()
+    cy.get('#siteListWrapper > main > article > footer > a > button').click()
     cy.get('#root > section > section > section > section > div > input').type(
       `TEST INSTANCE ${timeStamp}`
     )
     cy.get(
       '#root > section > section > section > section > div > div > button'
     ).click()
+    cy.wait(10000)
+    cy.get(
+      '#root > section > section.AppMain.AppMain > section > div > section > main > article:nth-child(1) > footer > button'
+    ).click()
+    cy.pause()
+    cy.get('.Name').should('contain', timeStamp)
   })
+
   // use the timeStamp to delete the same instance
 })
