aa1eeafcec8bca0cf43c9da2c3ed954cab2e636f
Grant Glidewell <grant.glidewell@zesty.io>
2018-07-12 13:57:58 -0700

    added: nav spec

--- cypress/integration/navigation_spec.js
+++ cypress/integration/navigation_spec.js
@@ -3,6 +3,18 @@ describe('App Navigation', () => {
 
   it('Navigates through the app', () => {
     cy.login(Cypress.env('validEmail'), Cypress.env('validPassword'))
-    // navigate to instances, blueprints, user settings, support, logout
+    // navigate to user settings
+    cy.get('#root > section > header > nav.UserNav.UserNav').click({
+      force: true
+    })
+    cy.get('#userNavDropdown > li:nth-child(3) > a').click()
+    // navigate to Support
+    cy.get('#root > section > header > nav.UserNav.UserNav').click({
+      force: true
+    })
+    cy.get('#userNavDropdown > li:nth-child(4) > a').click()
+
+    cy.get('#blueprintsNavLink').click()
+    cy.get('#instancesNavLink').click()
   })
 })
