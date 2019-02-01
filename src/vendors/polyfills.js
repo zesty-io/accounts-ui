35f9f955ea4619aab628267818ca294c52e3cb62
Stuart Runyan <shrunyan@gmail.com>
2019-01-31 17:00:23 -0800

    fixed: closest dom function. added polyfill for IE11 support

--- src/vendors/polyfills.js
+++ src/vendors/polyfills.js
@@ -7,3 +7,23 @@ import 'core-js/fn/string/includes'
 import 'core-js/fn/array/includes'
 import 'core-js/fn/promise'
 import 'whatwg-fetch'
+
+// closest
+// @see https://developer.mozilla.org/en-US/docs/Web/API/Element/closest#Polyfill
+if (!Element.prototype.matches) {
+  Element.prototype.matches =
+    Element.prototype.msMatchesSelector ||
+    Element.prototype.webkitMatchesSelector
+}
+
+if (!Element.prototype.closest) {
+  Element.prototype.closest = function(s) {
+    var el = this
+
+    do {
+      if (el.matches(s)) return el
+      el = el.parentElement || el.parentNode
+    } while (el !== null && el.nodeType === 1)
+    return null
+  }
+}
