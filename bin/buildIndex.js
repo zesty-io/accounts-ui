57f63f9423906ce2044135266e016f9a7dd548c2
Randy Apuzzo <apuzzo@gmail.com>
2019-04-25 13:19:04 -0700

    Added GA tag to the head

--- bin/buildIndex.js
+++ bin/buildIndex.js
@@ -129,6 +129,17 @@ module.exports = buildIndex = build => {
           document.head.appendChild(script);
         }
       </script>
+
+      <!-- Global site tag (gtag.js) - Google Analytics -->
+      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-21247490-20"></script>
+      <script>
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'UA-21247490-20');
+      </script>
+
     </head>
 
     <body>
