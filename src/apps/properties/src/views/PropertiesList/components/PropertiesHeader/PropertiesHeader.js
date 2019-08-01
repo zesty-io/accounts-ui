214f3fbf7233a827c7ef7b34a683dbc10c156c06
Stuart Runyan <shrunyan@gmail.com>
2019-08-01 10:36:55 -0700

    organize code stylisticly

--- src/apps/properties/src/views/PropertiesList/components/PropertiesHeader/PropertiesHeader.js
+++ src/apps/properties/src/views/PropertiesList/components/PropertiesHeader/PropertiesHeader.js
@@ -12,129 +12,131 @@ import { ButtonGroup } from '@zesty-io/core/ButtonGroup'
 import { Button } from '@zesty-io/core/Button'
 
 import styles from './PropertiesHeader.less'
-class PropertiesHeader extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      eco: this.props.settings && this.props.settings.eco,
-      sort: 'name'
+
+export default connect(state => state)(
+  class PropertiesHeader extends Component {
+    constructor(props) {
+      console.log('PropertiesHeader', props)
+
+      super(props)
+      this.state = {
+        eco: props.settings && props.settings.eco,
+        sort: 'name'
+      }
     }
-  }
-  render() {
-    const ecosystems = this.props.ecosystems
-      // .filter(eco => eco.id !== this.state.eco)
-      .map(eco => {
+
+    onSearch = debounce(term => {
+      this.setState(
+        {
+          searchTerm: term
+        },
+        () =>
+          this.props.dispatch({
+            type: 'SETTING_FILTER',
+            filter: this.state.searchTerm
+          })
+      )
+    }, 300)
+
+    filterByEco = (name, value) => {
+      if (value) {
+        this.setState({ eco: value })
+        this.props.dispatch({
+          type: 'SETTING_ECO',
+          eco: Number(value)
+        })
+      } else {
+        this.setState({ eco: false })
+        this.props.dispatch({
+          type: 'SETTING_ECO',
+          eco: ''
+        })
+      }
+    }
+
+    sort = by => {
+      this.props.dispatch(sortSites(by))
+    }
+
+    render() {
+      const ecosystems = this.props.ecosystems.map(eco => {
         return {
           value: eco.id,
           text: eco.name
         }
       })
 
-    return (
-      <header className={styles.PropertiesHeader}>
-        <div className={styles.Actions}>
-          {ecosystems.length && (
-            <DropDownFieldType
-              className={styles.Ecosystem}
-              name="ecoFilter"
-              onChange={this.filterByEco}
-              selection={ecosystems.find(eco => eco.id == this.state.eco)}
-              options={ecosystems}
+      return (
+        <header className={styles.PropertiesHeader}>
+          <div className={styles.Actions}>
+            {ecosystems.length && (
+              <DropDownFieldType
+                className={styles.Ecosystem}
+                name="ecoFilter"
+                onChange={this.filterByEco}
+                selection={ecosystems.find(eco => eco.id == this.state.eco)}
+                options={ecosystems}
+              />
+            )}
+
+            <Search
+              className={styles.Search}
+              override={this.props.settings && this.props.settings.filter}
+              placeholder="Search by instance name or domain"
+              onSubmit={this.onSearch}
+              onKeyUp={this.onSearch}
             />
-          )}
 
-          <Search
-            className={styles.Search}
-            override={this.props.settings && this.props.settings.filter}
-            placeholder="Search by instance name or domain"
-            onSubmit={this.onSearch}
-            onKeyUp={this.onSearch}
-          />
+            <ButtonGroup className={styles.Sort}>
+              <Button
+                title="Sort alphabetically by name"
+                disabled={this.state.sort === 'name'}
+                onClick={() => {
+                  this.setState({ sort: 'name' })
+                  return this.sort('name')
+                }}>
+                <i className={`fas fa-sort-alpha-down`} />
+              </Button>
+              <Button
+                title="Sort by created date"
+                disabled={this.state.sort === 'date'}
+                onClick={() => {
+                  this.setState({ sort: 'date' })
+                  return this.sort('createdAt')
+                }}>
+                <i className={`fas fa-calendar`} />
+              </Button>
+            </ButtonGroup>
 
-          <ButtonGroup className={styles.Sort}>
-            <Button
-              title="Sort alphabetically by name"
-              disabled={this.state.sort === 'name'}
-              onClick={() => {
-                this.setState({ sort: 'name' })
-                return this.sort('name')
-              }}>
-              <i className={`fas fa-sort-alpha-down`} />
-            </Button>
-            <Button
-              title="Sort by created date"
-              disabled={this.state.sort === 'date'}
-              onClick={() => {
-                this.setState({ sort: 'date' })
-                return this.sort('createdAt')
-              }}>
-              <i className={`fas fa-calendar`} />
-            </Button>
-          </ButtonGroup>
-
-          <ButtonGroup className={styles.Layout}>
-            <Button
-              title="View instances as a grid"
-              disabled={this.props.layout === 'grid'}
-              onClick={() => {
-                this.props.dispatch({
-                  type: 'INSTANCE_LAYOUT',
-                  layout: 'grid'
-                })
-                this.props.dispatch(saveProfile())
-              }}>
-              <i className={`fa fa-th`} />
-            </Button>
-            <Button
-              title="View instances as a list"
-              disabled={this.props.layout === 'list'}
-              onClick={() => {
-                this.props.dispatch({
-                  type: 'INSTANCE_LAYOUT',
-                  layout: 'list'
-                })
-                this.props.dispatch(saveProfile())
-              }}>
-              <i className={`fa fa-th-list`} />
-            </Button>
-          </ButtonGroup>
-        </div>
-      </header>
-    )
-  }
-
-  onSearch = debounce(term => {
-    this.setState(
-      {
-        searchTerm: term
-      },
-      () =>
-        this.props.dispatch({
-          type: 'SETTING_FILTER',
-          filter: this.state.searchTerm
-        })
-    )
-  }, 300)
-
-  filterByEco = (name, value) => {
-    if (value) {
-      this.setState({ eco: value })
-      this.props.dispatch({
-        type: 'SETTING_ECO',
-        eco: Number(value)
-      })
-    } else {
-      this.setState({ eco: false })
-      this.props.dispatch({
-        type: 'SETTING_ECO',
-        eco: ''
-      })
+            <ButtonGroup className={styles.Layout}>
+              <Button
+                title="View instances as a grid"
+                disabled={this.props.layout === 'grid'}
+                onClick={() => {
+                  this.props.dispatch({
+                    type: 'INSTANCE_LAYOUT',
+                    layout: 'grid'
+                  })
+                  this.props.dispatch(saveProfile())
+                }}>
+                <i className={`fa fa-th`} />
+              </Button>
+              <Button
+                title="View instances as a list"
+                disabled={this.props.layout === 'list'}
+                onClick={() => {
+                  this.props.dispatch({
+                    type: 'INSTANCE_LAYOUT',
+                    layout: 'list'
+                  })
+                  this.props.dispatch(saveProfile())
+                }}>
+                <i className={`fa fa-th-list`} />
+              </Button>
+            </ButtonGroup>
+          </div>
+        </header>
+      )
     }
   }
-
-  sort = by => {
-    this.props.dispatch(sortSites(by))
-  }
-}
-
-export default connect(state => state)(PropertiesHeader)
+)
