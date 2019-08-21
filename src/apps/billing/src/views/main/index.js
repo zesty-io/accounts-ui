55276b2667848ad8d2ccea2deaf794e7ebb3517c
Stuart Runyan <shrunyan@gmail.com>
2019-08-21 15:46:51 -0700

    WIP: new layout after design review

--- src/apps/billing/src/views/main/index.js
+++ src/apps/billing/src/views/main/index.js
@@ -4,6 +4,8 @@ import { connect } from 'react-redux'
 import { Route, Switch } from 'react-router-dom'
 
 import { Card, CardHeader, CardContent, CardFooter } from '@zesty-io/core/Card'
+import { DropDownFieldType } from '@zesty-io/core/DropDownFieldType'
+import { ButtonGroup } from '@zesty-io/core/ButtonGroup'
 import { Button } from '@zesty-io/core/Button'
 
 import styles from './Billing.less'
@@ -21,290 +23,69 @@ export default function Billing(props) {
         how it is billed.
       </p>
 
-      <div className={styles.CreditCards}>
-        <Card className={styles.CreditCard}>
-          {/* <CardHeader>Ecosystem 2</CardHeader> */}
-          <CardContent className={styles.AddBilling}>
-            <h1 className={styles.display}>Instance Billing</h1>
-            <p>
-              Billing is per Ecosystem and Ecosytems can contain many instances.
-              When creating an instance an Ecosystem will be automatically
-              created for you.
-            </p>
-            <Button
-              kind="save"
-              onClick={() => console.log('//TODO add credit card')}>
-              <i className="fa fa-trash"></i>Add Billing
-            </Button>
-          </CardContent>
-          {/* <CardFooter></CardFooter> */}
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-edit"></i>Edit
-            </Button>
-            <Button
-              kind="warn"
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-        <Card className={styles.CreditCard}>
-          <CardHeader>Ecosystem 1</CardHeader>
-          <CardContent className={styles.CardContent}>
-            <p>
-              Card Holder: <strong>Stuart H. Runyan</strong>
-            </p>
-            <p>
-              Card Number: <strong>0000-0000-0000-0000</strong>
-            </p>
-            <p>
-              Card Experation: <strong>03/22</strong>
-            </p>
-          </CardContent>
-          <CardFooter>
-            <Button
-              onClick={() => console.log('//TODO remove credit card onfile')}>
-              <i className="fa fa-trash"></i>Remove
-            </Button>
-          </CardFooter>
-        </Card>
-      </div>
+      <header>
+        <DropDownFieldType
+          className={styles.ecosystem}
+          name="ecosystem"
+          options={[
+            {
+              value: '16-000-0000',
+              text: 'My Ecosystem'
+            }
+          ]}
+          onChange={() => {
+            console.log('//TODO load selected ecosystem')
+          }}
+        />
+        <h1 className={cx(styles.total, styles.display)}>
+          $000.00 / 08-21-2019
+        </h1>
+      </header>
+
+      <main>
+        <div className={styles.row}>
+          <Card className={styles.Card}>
+            <CardHeader>Payment Sources</CardHeader>
+            <CardContent>
+              <div>Visa ...0000 01/21</div>
+            </CardContent>
+            <CardFooter>
+              <ButtonGroup>
+                <Button kind="secondary">Add Card</Button>
+                <Button kind="secondary">Add Bank</Button>
+              </ButtonGroup>
+            </CardFooter>
+          </Card>
+          <Card className={styles.Card}>
+            <CardHeader>Billing Address</CardHeader>
+            <CardContent>
+              <h1>My Business</h1>
+              <div>0000 Main St., San Diego, CA, 92101</div>
+            </CardContent>
+            <CardFooter>
+              <Button>Edit</Button>
+            </CardFooter>
+          </Card>
+        </div>
+        <div className={styles.row}>
+          <Card className={styles.Card}>
+            <CardHeader>Invoice Summary</CardHeader>
+            <CardContent>
+              <div>TBD</div>
+            </CardContent>
+          </Card>
+        </div>
+        <div className={styles.row}>
+          <Card className={styles.Card}>
+            <CardHeader>Instances</CardHeader>
+            <CardContent>
+              <div>
+                Name | Date Created <Button kind="warn">Remove</Button>
+              </div>
+            </CardContent>
+          </Card>
+        </div>
+      </main>
     </section>
   )
 }
