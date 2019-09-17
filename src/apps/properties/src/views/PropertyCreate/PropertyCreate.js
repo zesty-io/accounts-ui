97b376a9534c84444169fbd47e27e899c39119f7
Stuart Runyan <shrunyan@gmail.com>
2019-09-17 15:19:41 -0700

    added: display ecosystem selector on instance creation

--- src/apps/properties/src/views/PropertyCreate/PropertyCreate.js
+++ src/apps/properties/src/views/PropertyCreate/PropertyCreate.js
@@ -1,23 +1,36 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
 import { Link } from 'react-router-dom'
 
 import { createInstance } from '../../store/sites'
 import { notify } from '../../../../../shell/store/notifications'
+import { fetchEcosystems } from '../../../../../shell/store/ecosystems'
 
 import { Input } from '@zesty-io/core/Input'
 import { Button } from '@zesty-io/core/Button'
+import { Infotip } from '@zesty-io/core/Infotip'
+import { DropDownFieldType } from '@zesty-io/core/DropDownFieldType'
 
 import styles from './PropertyCreate.less'
-export default connect()(
-  class PropertyCreate extends Component {
+export default connect(state => {
+  return {
+    ecosystems: state.ecosystems
+  }
+})(
+  class PropertyCreate extends React.Component {
     constructor(props) {
       super(props)
       this.state = {
         submitted: false,
+        ecoZUID: '',
         name: ''
       }
     }
+
+    componentDidMount() {
+      this.props.dispatch(fetchEcosystems())
+    }
+
     render() {
       return (
         <section className={styles.PropertyCreate}>
@@ -29,6 +42,35 @@ export default connect()(
               placeholder="e.g. Internal API, Marketing Website, etc"
               onChange={this.handleChange}
             />
+
+            {this.props.ecosystems.length ? (
+              <React.Fragment>
+                <h2>
+                  <Infotip>
+                    By adding an instance to an EcoSystem you allow sharing
+                    assets between instances within the same EcoSystem.
+                  </Infotip>
+                  &nbsp;EcoSystem
+                </h2>
+                <DropDownFieldType
+                  className={styles.Ecosystem}
+                  name="ecoFilter"
+                  onChange={(name, value) => {
+                    this.setState({
+                      ecoZUID: value
+                    })
+                  }}
+                  // selection={ecosystems.find(eco => eco.id == this.state.eco)}
+                  options={this.props.ecosystems.map(eco => {
+                    return {
+                      value: eco.ZUID,
+                      text: eco.name
+                    }
+                  })}
+                />
+              </React.Fragment>
+            ) : null}
+
             <div className={styles.controls}>
               <Button
                 data-test="createInstance"
@@ -54,10 +96,12 @@ export default connect()(
       })
     }
     handleClick = () => {
+      console.log('handleClick', this.state)
+
       if (this.state.name) {
         this.setState({ submitted: true })
         this.props
-          .dispatch(createInstance(this.state.name))
+          .dispatch(createInstance(this.state.name, this.state.ecoZUID))
           .then(site => {
             this.setState({ submitted: false })
             this.props.history.push(`/instances/${site.ZUID}/blueprint`)
