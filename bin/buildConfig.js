7fc4ff3306d78750721232789eb087b0deab413c
Stuart Runyan <shrunyan@gmail.com>
2020-03-02 14:37:25 -0800

    change: auth service url

--- bin/buildConfig.js
+++ bin/buildConfig.js
@@ -50,7 +50,7 @@ module.exports = buildConfig = build => {
     config = {
       API_ACCOUNTS: 'http://accounts.api.zesty.localdev:3022/v1',
       API_INSTANCE: '.api.zesty.localdev:3023/v1/',
-      API_AUTH: 'http://svc.zesty.localdev:3011/auth',
+      API_AUTH: 'http://auth.zesty.localdev:3011',
       MANAGER_URL: '.manage.zesty.localdev:3020',
       MANAGER_URL_PROTOCOL: 'http://',
       PREVIEW_URL: '-dev.preview.zestyio.localdev:3020',
