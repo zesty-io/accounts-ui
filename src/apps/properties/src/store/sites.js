b8713141beadb339d799e764122a67d014b962d2
Joshua Hanson <joshua.hanson@zesty.io>
2021-02-11 12:53:47 -0600

    null check same line

--- src/apps/properties/src/store/sites.js
+++ src/apps/properties/src/store/sites.js
@@ -44,16 +44,10 @@ export function fetchSites() {
     })
     return request(`${CONFIG.API_ACCOUNTS}/instances`).then(sites => {
       sites.data.sort((prev, next) => {
-        if (prev.name === null) {
+        if (prev.name === null || prev.name < next.name) {
           return -1
         }
-        if (next.name === null) {
-          return 1
-        }
-        if (prev.name < next.name) {
-          return -1
-        }
-        if (prev.name > next.name) {
+        if (next.name === null || prev.name > next.name) {
           return 1
         }
         return 0
