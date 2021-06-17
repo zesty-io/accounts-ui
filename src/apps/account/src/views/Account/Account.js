9cdd2e97f3a442bfe9ee40b4d3aff033fa754046
David Naimi <d88naimi@gmail.com>
2021-06-17 10:42:52 -0700

    converting class to functional component and hiding password and 2fa for sso users

--- src/apps/account/src/views/Account/Account.js
+++ src/apps/account/src/views/Account/Account.js
@@ -1,5 +1,6 @@
-import React, { Component } from 'react'
+import React, { useState, useEffect } from 'react'
 import { connect } from 'react-redux'
+import useIsMounted from 'ismounted'
 
 import { fetchUserEmails, saveProfile } from '../../../../../shell/store/user'
 import { notify } from '../../../../../shell/store/notifications'
@@ -14,104 +15,102 @@ import { Preferences } from './components/Preferences'
 import { WithLoader } from '@zesty-io/core/WithLoader'
 
 import styles from './Account.less'
-export default connect(state => state)(
-  class Account extends Component {
-    state = {
-      loadingEmails: true
-    }
-    componentDidMount() {
-      this.props
-        .dispatch(fetchUserEmails())
-        .then(data => {
-          this.setState({
-            loadingEmails: false
-          })
-        })
-        .catch(err => {
-          this.props.dispatch(
-            notify({
-              message: `Error fetching emails`,
-              type: 'error'
-            })
-          )
-        })
 
-      if (!this.props.user.prefs.hasSelectedDev) {
-        this.props.dispatch(
-          zConfirm({
-            prompt:
-              'Are you interested in using developer features, such as access to blueprints? You can change this any time in your account settings.',
-            callback: response => {
-              if (response) {
-                this.props.dispatch({
-                  type: 'DEV_PREFS',
-                  payload: 1
-                })
-                this.props.dispatch(
-                  saveProfile({
-                    websiteCreator: true
-                  })
-                )
-              } else {
-                this.props.dispatch({
-                  type: 'DEV_PREFS',
-                  payload: 0
-                })
-                this.props.dispatch(
-                  saveProfile({
-                    websiteCreator: false
-                  })
-                )
-              }
-            }
+export default connect(state => {})(function Account(props) {
+  const [loadingEmails, setLoadingEmails] = useState(true)
+  const isMounted = useIsMounted()
+
+  useEffect(() => {
+    props
+      .dispatch(fetchUserEmails())
+      .then(data => {
+        if (isMounted.current) {
+          setLoadingEmails(false)
+        }
+      })
+      .catch(err => {
+        props.dispatch(
+          notify({
+            message: `Error fetching emails`,
+            type: 'error'
           })
         )
-      }
-    }
-    render() {
-      document.title = 'Accounts: My Account'
-      return (
-        <section className={styles.Settings}>
-          <h1 className={styles.SettingsTitle}>Manage Your Account Settings</h1>
-          <div className={styles.setting}>
-            <WithLoader
-              className={styles.Loading}
-              condition={!this.state.loadingEmails}
-              message="Loading Your Account Data">
-              <div className={styles.SettingCards}>
-                <Profile className={styles.SettingCard} />
-                <Email
-                  className={styles.SettingCard}
-                  dispatch={this.props.dispatch}
-                  user={this.props.user}
-                  emails={this.props.user.emails}
-                  loadingEmails={this.state.loadingEmails}
-                />
-                {this.props.user.authSource === null ? (
-                  <>
-                    <Password
-                      history={this.props.history}
-                      className={styles.SettingCard}
-                    />
-                    <TwoFactor
-                      className={styles.SettingCard}
-                      dispatch={this.props.dispatch}
-                      user={this.props.user}
-                    />
-                  </>
-                ) : (
-                  ''
-                )}
-                <Preferences
-                  className={styles.SettingCard}
-                  user={this.props.user}
-                  dispatch={this.props.dispatch}
-                />
-              </div>
-            </WithLoader>
-          </div>
-        </section>
+      })
+
+    if (props.user.prefs.hasSelectedDev) {
+      props.dispatch(
+        zConfirm({
+          prompt:
+            'Are you interested in using developer features, such as access to blueprints? You can change this any time in your account settings.',
+          callback: response => {
+            if (response) {
+              props.dispatch({
+                type: 'DEV_PREFS',
+                payload: 1
+              })
+              props.dispatch(
+                saveProfile({
+                  websiteCreator: true
+                })
+              )
+            } else {
+              props.dispatch({
+                type: 'DEV_PREFS',
+                payload: 0
+              })
+              props.dispatch(
+                saveProfile({
+                  websiteCreator: false
+                })
+              )
+            }
+          }
+        })
       )
     }
-  }
-)
+  }, [])
+
+  document.title = 'Accounts: My Account'
+  return (
+    <section className={styles.Settings}>
+      <h1 className={styles.SettingsTitle}>Manage Your Account Settings</h1>
+      <div className={styles.setting}>
+        <WithLoader
+          className={styles.Loading}
+          condition={!loadingEmails}
+          message="Loading Your Account Data">
+          <div className={styles.SettingCards}>
+            <Profile className={styles.SettingCard} />
+            <Email
+              className={styles.SettingCard}
+              dispatch={props.dispatch}
+              user={props.user}
+              emails={props.user.emails}
+              loadingEmails={loadingEmails}
+            />
+            {props.user.authSource === null ? (
+              <>
+                <Password
+                  history={props.history}
+                  className={styles.SettingCard}
+                />
+                <TwoFactor
+                  className={styles.SettingCard}
+                  dispatch={props.dispatch}
+                  user={props.user}
+                />
+              </>
+            ) : (
+              ''
+            )}
+            <Preferences
+              className={styles.SettingCard}
+              user={props.user}
+              dispatch={props.dispatch}
+            />
+          </div>
+        </WithLoader>
+      </div>
+    </section>
+  )
+})
