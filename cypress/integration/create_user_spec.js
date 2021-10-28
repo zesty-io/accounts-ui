035e68449b6c33add3afbe3d02deffacf5a7edb0
faris huskovic <farishuskovic@fariss-MBP.attlocal.net>
2021-10-28 17:37:49 -0400

    fix create user tests

--- cypress/integration/create_user_spec.js
+++ cypress/integration/create_user_spec.js
@@ -6,15 +6,11 @@ describe('User Creation Flow', function() {
     cy.get('[name=email]').type(`newUser${Date.now()}@email.com`)
     cy.get('[name=firstName]').type('newUserName')
     cy.get('[name=lastName]').type('lastName')
-    cy.get('[name=pass]').type('asdASD123')
+    cy.get('input[type=password]').type('asdASD123')
     cy.get('[name=eula]').check({ force: true })
 
     // Submit signup form
     cy.get('button[name=submit]').click()
-
-    cy.get('#root > section > main > p', { timeout: 15000 }).should(
-      'contain',
-      'We have sent a verification email from donotreply@system.zesty.email to the email address you signed up with. This is done for account security to ensure you are the owner of the email address. If you do not see a verification email in your inbox, make sure to check your spam folder.'
-    )
+    cy.url().should('contain', 'instances', { timeout: 5000 })
   })
 })
