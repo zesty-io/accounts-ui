c82af9cb166b16cb9db120247b7719f4a843379f
David Naimi <d88naimi@gmail.com>
2022-02-28 14:06:20 -0800

    seperate file

--- src/apps/properties/src/views/PropertyOverview/components/Domain/Validator.js
+++ src/apps/properties/src/views/PropertyOverview/components/Domain/Validator.js
@@ -1,3 +1,5 @@
+//If this function is updated please update cypress test in domain_validator.js
+
 export const Validator = url => {
   return /^[a-zA-Z0-9][a-zA-Z0-9-]{1,61}[a-zA-Z0-9](?:\.[a-zA-Z]{2,})+$/.test(
     url
